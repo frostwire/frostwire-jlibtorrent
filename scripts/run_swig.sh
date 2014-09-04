ccb660fc1d90b51627e018528c7e64a879b3d9cf
Alden Torres <aldenml@gmail.com>
2014-09-04 11:27:51 -0400

    Working run_swig.sh script.

--- scripts/run_swig.sh
+++ scripts/run_swig.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-JAVA_SRC_OUTPUT=java/src/com/frostwire/libtorrent/swig
+JAVA_SRC_OUTPUT=src/com/frostwire/libtorrent/swig
 
-rm -rf $JAVA_SRC_OUTPUT
-mkdir -p $JAVA_SRC_OUTPUT
+rm -rf ${JAVA_SRC_OUTPUT}
+mkdir -p ${JAVA_SRC_OUTPUT}
 
-swig -c++ -java -o libtorrent_jni.cpp -outdir $JAVA_SRC_OUTPUT -package com.frostwire.libtorrent.swig \
+swig -c++ -java -o libtorrent_jni.cpp \
+    -outdir ${JAVA_SRC_OUTPUT} \
+    -package com.frostwire.libtorrent.swig \
     -Ilibtorrent-rasterbar-1.0.1/include \
     -I/usr/local/include \
     -DBOOST_ASIO_DECL="" \
@@ -23,7 +25,7 @@ swig -c++ -java -o libtorrent_jni.cpp -outdir $JAVA_SRC_OUTPUT -package com.fros
     swig/libtorrent.i
 
 CC="clang++"
-INCLUDES="-I/usr/local/include -I/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/include/ -I/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/include/darwin"
+INCLUDES="-I/usr/local/include -I/Library/Java/JavaVirtualMachines/jdk1.8.0_20.jdk/Contents/Home/include/ -I/Library/Java/JavaVirtualMachines/jdk1.8.0_20.jdk/Contents/Home/include/darwin"
 LIBS="-ltorrent-rasterbar -lboost_system"
 CFLAGS="-stdlib=libc++ -std=c++11 -O3 -DBOOST_ASIO_DYN_LINK=1 -DTORRENT_NO_DEPRECATE=1 -DTORRENT_DISABLE_GEO_IP=1"
 LDFLAGS="-L/usr/local/lib"
@@ -31,3 +33,5 @@ TARGET="jlibtorrent.dylib"
 
 $CC $CFLAGS $INCLUDES -c libtorrent_jni.cpp
 $CC $LDFLAGS $LIBS -dynamiclib -o $TARGET libtorrent_jni.o
+
+rm -rf libtorrent_jni.o
