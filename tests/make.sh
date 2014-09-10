e38e5ecbc283da5101cb662bfe7634d9dcf6e740
gubatron <gubatron@gmail.com>
2014-09-10 18:09:06 -0400

    builds and runs (without segfault in linux)

--- tests/make.sh
+++ tests/make.sh
@@ -3,19 +3,21 @@
 set -e 
 #print commands
 set -x 
-CC=g++
 
-INCLUDES="-I/usr/local/includes/"
 LINK_LIBS="-ltorrent-rasterbar -lboost_system -lboost_filesystem"
-LINKER_PATHS="-L/usr/local/Cellar/boost/HEAD/lib"
 STD="-std=c++11"
-STD_LIB="-stdlib=libstdc++"
 
-if [[ $OSTYPE == darwin* ]] ; then
+if [[ "$OSTYPE" == "darwin"* ]]; then
   CC=clang++
-  STD="-std=c++11"
   STD_LIB="-stdlib=libc++"
   INCLUDES="-I/usr/local/libtorrent-rasterbar/1.0.1_1/include"
+  LINKER_PATHS="-L/usr/local/Cellar/boost/HEAD/lib"
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+  CC=g++
+  STD_LIB=""
+  INCLUDES="-I/usr/local/includes"
+  LINKER_PATHS="-L/usr/local/lib"
+  export LD_LIBRARY_PATH="/usr/local/lib"
 fi
 
 CPP_FILES="api_walkthrough.cpp"
@@ -24,3 +26,4 @@ OUTPUT="api_walkthrough"
 FLAGS="-Wfatal-errors ${STD} ${STD_LIB} -DBOOST_ASIO_SEPARATE_COMPILATION -g"
 
 ${CC} ${FLAGS} ${INCLUDES} ${CPP_FILES} -o ${OUTPUT} ${LINK_LIBS} ${LINKER_PATHS}
+
