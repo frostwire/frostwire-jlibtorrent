2787a601e79594abc735b8877d9edacc734d58e2
Alden Torres <aldenml@gmail.com>
2014-09-12 09:13:23 -0400

    Added bencode function.

--- src/com/frostwire/jlibtorrent/LibTorrent.java
+++ src/com/frostwire/jlibtorrent/LibTorrent.java
@@ -1,5 +1,7 @@
 package com.frostwire.jlibtorrent;
 
+import com.frostwire.jlibtorrent.swig.char_vector;
+import com.frostwire.jlibtorrent.swig.entry;
 import com.frostwire.jlibtorrent.swig.libtorrent;
 
 public final class LibTorrent {
@@ -13,6 +15,19 @@ public final class LibTorrent {
 
     public static String version() {
         return libtorrent.LIBTORRENT_VERSION;
+    }
+
+    public static byte[] bencode(entry e) {
+        char_vector vector = libtorrent.entry_bencode(e);
+
+        int size = (int) vector.size();
+        byte[] arr = new byte[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = (byte) vector.get(i);
+        }
+
+        return arr;
     }
 
     static long time2millis(int time) {
