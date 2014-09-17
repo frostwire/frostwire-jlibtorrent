74e03bf5e0ea2cc883e6afefc2e33f04ffa9fccf
Alden Torres <aldenml@gmail.com>
2014-09-17 13:24:27 -0400

    Demo using LazyRead (vey buggy).

---
+++ src/com/frostwire/jlibtorrent/demo/LazyRead.java
@@ -0,0 +1,61 @@
+/*
+ * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
+ * Copyright (c) 2011-2014, FrostWire(R). All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.frostwire.jlibtorrent.demo;
+
+import com.frostwire.jlibtorrent.LibTorrent;
+import com.frostwire.jlibtorrent.swig.char_vector;
+import com.frostwire.jlibtorrent.swig.error_code;
+import com.frostwire.jlibtorrent.swig.lazy_entry;
+import com.frostwire.jlibtorrent.swig.torrent_info;
+
+import java.io.File;
+import java.nio.file.Files;
+
+/**
+ * @author gubatron
+ * @author aldenml
+ */
+public final class LazyRead {
+
+    public static void main(String[] args) throws Throwable {
+
+        // comment this line for a real application
+        args = new String[]{"/Users/aldenml/Downloads/Kellee_Maize_The_5th_Element_FrostClick_FrostWire_MP3_April_14_2014.torrent"};
+
+        File torrentFile = new File(args[0]);
+
+        System.out.println("Using libtorrent version: " + LibTorrent.version());
+
+        byte[] data = Files.readAllBytes(torrentFile.toPath());
+
+        char_vector buffer = LibTorrent.bytes2char_vector(data);
+        lazy_entry e = new lazy_entry();
+        error_code ec = new error_code();
+        int ret = lazy_entry.bdecode(buffer, e, ec);
+
+        if (ret != 0) {
+            System.out.println("failed to decode torrent: " + ec.message());
+            return;
+        }
+
+        torrent_info ti = new torrent_info(e);
+
+        System.out.println(LibTorrent.info_hash2string(ti.info_hash()));
+    }
+}
