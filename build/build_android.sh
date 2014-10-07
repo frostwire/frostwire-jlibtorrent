371af034bb2a8799933afb2a9b4e24df1e709688
Alden Torres <aldenml@gmail.com>
2014-10-07 16:10:30 -0400

    Working in android build script (not working). Cleanup.

--- build/build_android.sh
+++ build/build_android.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+#make-standalone-toolchain.sh --platform=android-15 --arch=arm  --toolchain=arm-linux-androideabi-4.8 --install-dir=toolchain-platform-15-arm-gcc-4.8
+#source android-setup-env.sh
+
+#define
+#$CXX
+#$BOOST_ROOT
+#$LIBTORRENT_ROOT
+#$JDK_INCLUDE_1
+#$JDK_INCLUDE_2
+#$LBITORRENT_LIBS
+#$ANDROID_ARM_TOOLCHAIN_ROOT
+
+#export CXXFLAGS="-mthumb -fno-strict-aliasing -lstdc++ -D__GLIBC__ -D_GLIBCXX__PTHREADS -D__arm__ -D_REENTRANT -O3"
+#copy user-config.jam to ~/
+#$BOOST_ROOT/b2 toolset=gcc variant=release link=static
+#$BOOST_ROOT/bjam toolset=gcc-androidarm variant=release deprecated-functions=off link=static
+
 CXXFLAGS="-mthumb -fno-strict-aliasing -lstdc++ -D__GLIBC__ -D_GLIBCXX__PTHREADS -D__arm__ -D_REENTRANT -O3"
 DEFINES="-DNDEBUG=1 -DBOOST_ASIO_SEPARATE_COMPILATION=1 -DTORRENT_USE_CLOCK_GETTIME=1 -DTORRENT_DISABLE_GEO_IP=1 -DTORRENT_NO_DEPRECATE=1"
 INCLUDES="-I$BOOST_ROOT -I$LIBTORRENT_ROOT/include -I$JDK_INCLUDE_1 -I$JDK_INCLUDE_2"
@@ -7,7 +24,7 @@ LIBS="-ltorrent -lboost_system -lboost_chrono -lboost_date_time -lboost_thread"
 LDFLAGS="-Wl,-Bsymbolic -L$LBITORRENT_LIBS"
 TARGET="libjlibtorrent.so"
 
-$CXX $CXXFLAGS $INCLUDES -c swig/libtorrent_jni.cpp
+$CXX $CXXFLAGS $DEFINES $INCLUDES -c swig/libtorrent_jni.cpp
 $CXX -shared -o $TARGET libtorrent_jni.o $LIBS $LDFLAGS
 
 rm -rf libtorrent_jni.o
--- build/build_windows.bat
+++ build/build_windows.bat
@@ -1,3 +1,8 @@
+rem Review this
+rem set CXXFLAGS=/O2 /fp:fast /DTORRENT_USE_IPV6=1 /DNDEBUG=1 /I"C:\boost_1_56_0"
+rem bjam toolset=msvc variant=release deprecated-functions=off link=static runtime-link=static
+
+
 set LIBS=advapi32.lib
 set LOPT=/RELEASE /NOLOGO /MACHINE:X86 /ENTRY:_DllMainCRTStartup@12 /DLL
 set CFLAGS=/O2 /fp:fast /nologo /DWIN32 /EHsc /c /DBOOST_ASIO_SEPARATE_COMPILATION=1 /DTORRENT_USE_IPV6=1 /DNDEBUG=1 /DTORRENT_NO_DEPRECATE=1 /DTORRENT_DISABLE_GEO_IP=1
@@ -7,3 +12,4 @@ set LIBTORRENT_INCLUDE=/I"C:\libtorrent-rasterbar-1.0.2\include"
 
 cl %CFLAGS% %JAVA_INCLUDE% %BOOST_INCLUDE% %LIBTORRENT_INCLUDE% swig\libtorrent_jni.cpp
 link %LOPT% /OUT:jlibtorrent.dll %LIBS% libtorrent.lib  libtorrent_jni.obj
+upx
