64749e73a77b8718c1a806bdbc61a2aeba00db1f
Alden Torres <aldenml@gmail.com>
2014-10-10 14:31:54 -0400

    Added lines related to build in x86 android.

--- build/build_android.sh
+++ build/build_android.sh
@@ -6,14 +6,16 @@
 #$JDK_INCLUDE_2
 #$LIBTORRENT_LIBS
 
-#copy user-config.jam to ~/
+#copy user-config.jam to ~/ change it for intel if necessary
 #make-standalone-toolchain.sh --platform=android-15 --arch=arm --toolchain=arm-linux-androideabi-4.8 --install-dir=toolchain-platform-15-arm-gcc-4.8
-#export CXXFLAGS="-mthumb -fno-strict-aliasing -lstdc++ -O3 -D__GLIBC__ -D_GLIBCXX__PTHREADS -D__arm__ -D_REENTRANT -I$BOOST_ROOT"
-#$BOOST_ROOT/b2 toolset=gcc-arm variant=release link=static target-os=linux --stagedir=android stage
+#ARM: export CXXFLAGS="-mthumb -fno-strict-aliasing -lstdc++ -O3 -D__GLIBC__ -D_GLIBCXX__PTHREADS -D__arm__ -D_REENTRANT -I$BOOST_ROOT"
+#X86: export CXXFLAGS="-fno-strict-aliasing -lstdc++ -O3 -D__GLIBC__ -D_GLIBCXX__PTHREADS -D_REENTRANT -I$BOOST_ROOT"
+#$BOOST_ROOT/b2 toolset=gcc-arm variant=release link=static target-os=linux --stagedir="android-arm" stage
 #$BOOST_ROOT/bjam toolset=gcc-arm variant=release link=static target-os=linux deprecated-functions=off
 
-CXX=arm-linux-androideabi-g++
-CXXFLAGS="-mthumb -fno-strict-aliasing -lstdc++ -O3 -D__GLIBC__ -D_GLIBCXX__PTHREADS -D__arm__ -D_REENTRANT"
+#CXX=arm-linux-androideabi-g++
+CXX=i686-linux-android-g++
+# use the env one CXXFLAGS="-mthumb -fno-strict-aliasing -lstdc++ -O3 -D__GLIBC__ -D_GLIBCXX__PTHREADS -D__arm__ -D_REENTRANT"
 DEFINES="-DNDEBUG=1 -DBOOST_ASIO_SEPARATE_COMPILATION=1 -DTORRENT_USE_CLOCK_GETTIME=1 -DTORRENT_DISABLE_GEO_IP=1 -DTORRENT_NO_DEPRECATE=1"
 INCLUDES="-I$BOOST_ROOT -I$LIBTORRENT_ROOT/include -I$JDK_INCLUDE_1 -I$JDK_INCLUDE_2"
 LIBS="-ltorrent -lboost_system -lboost_chrono -lboost_date_time -lboost_thread"
