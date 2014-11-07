1b0798ce2a3ec4fc55c032a2563fe853303df969
Alden Torres <aldenml@gmail.com>
2014-11-07 11:40:13 -0500

    More on Entry API.

--- src/com/frostwire/jlibtorrent/Entry.java
+++ src/com/frostwire/jlibtorrent/Entry.java
@@ -1,15 +1,17 @@
 package com.frostwire.jlibtorrent;
 
-import com.frostwire.jlibtorrent.swig.entry;
-import com.frostwire.jlibtorrent.swig.entry_list;
-import com.frostwire.jlibtorrent.swig.string_entry_map;
+import com.frostwire.jlibtorrent.swig.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
- * The ``entry`` class represents one node in a bencoded hierarchy. It works as a
- * variant type, it can be either a list, a dictionary (``std::map``), an integer
+ * The Entry class represents one node in a bencoded hierarchy. It works as a
+ * variant type, it can be either a list, a dictionary, an integer
  * or a string.
  *
  * @author gubatron
@@ -39,6 +41,43 @@ public final class Entry {
         return Vectors.char_vector2bytes(e.bencode());
     }
 
+    public String string() {
+        return e.string();
+    }
+
+    public long integer() {
+        return e.integer();
+    }
+
+    public ArrayList<Entry> list() {
+        entry_vector v = e.list().to_vector();
+        int size = (int) v.size();
+
+        ArrayList<Entry> list = new ArrayList<Entry>(size);
+
+        for (int i = 0; i < size; i++) {
+            list.add(new Entry(v.get(i)));
+        }
+
+        return list;
+    }
+
+    public Map<String, Entry> dictionary() {
+        string_entry_map dict = e.dict();
+        string_vector keys = dict.keys();
+        int size = (int) keys.size();
+
+        Map<String, Entry> map = new HashMap<String, Entry>(size);
+
+        for (int i = 0; i < size; i++) {
+            String key = keys.get(i);
+            Entry value = new Entry(dict.get(key));
+            map.put(key, value);
+        }
+
+        return map;
+    }
+
     @Override
     public String toString() {
         return e.to_string();
@@ -46,6 +85,11 @@ public final class Entry {
 
     public static Entry bdecode(byte[] data) {
         return new Entry(entry.bdecode(Vectors.bytes2char_vector(data)));
+    }
+
+    public static Entry bdecode(File file) throws IOException {
+        byte[] data = Utils.readFileToByteArray(file);
+        return bdecode(data);
     }
 
     public static Entry fromList(List<?> list) {
