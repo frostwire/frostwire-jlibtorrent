a62caf1735112b01e7c3c89fe79fbaf1ead67f4a
Alden Torres <aldenml@gmail.com>
2014-12-03 19:50:40 -0500

    Facing application freeze issue related to internal issues inside boost IO in Windows. Very similar to a qBittorrent issue and fixed using boost 1.55. Updated Windows binary. Relevant links: https://github.com/qbittorrent/qBittorrent/issues/2003 https://github.com/boostorg/asio/commit/0484963a55bf109353922e1d5fdc2a218995d9e7 https://svn.boost.org/trac/boost/ticket/10674

--- build/build_windows.bat
+++ build/build_windows.bat
@@ -1,12 +1,12 @@
-rem set CXXFLAGS=/O2 /fp:fast /DNDEBUG=1 /I"C:\boost_1_56_0"
-rem bjam toolset=msvc variant=release link=static deprecated-functions=off
+rem set CXXFLAGS=/MT /O2 /fp:fast /DNDEBUG=1 /I"C:\boost_1_55_0"
+rem bjam toolset=msvc variant=release link=static runtime-link=static deprecated-functions=off
 
 
 set LIBS=advapi32.lib
 set LOPT=/RELEASE /NOLOGO /MACHINE:X86 /ENTRY:_DllMainCRTStartup@12 /DLL
-set CFLAGS=/O2 /fp:fast /nologo /DWIN32 /EHsc /c /DNDEBUG=1 /DBOOST_ASIO_SEPARATE_COMPILATION=1 /DTORRENT_DISABLE_GEO_IP=1 /DTORRENT_NO_DEPRECATE=1
+set CFLAGS=/MT /O2 /fp:fast /nologo /DWIN32 /EHsc /c /DNDEBUG=1 /DBOOST_ASIO_SEPARATE_COMPILATION=1 /DTORRENT_DISABLE_GEO_IP=1 /DTORRENT_NO_DEPRECATE=1
 set JAVA_INCLUDE=/I"C:\Program Files (x86)\Java\jdk1.8.0_25\include" /I"C:\Program Files (x86)\Java\jdk1.8.0_25\include\win32"
-set BOOST_INCLUDE=/I"C:\boost_1_56_0"
+set BOOST_INCLUDE=/I"C:\boost_1_55_0"
 set LIBTORRENT_INCLUDE=/I"C:\libtorrent-rasterbar-1.0.2\include"
 
 cl %CFLAGS% %JAVA_INCLUDE% %BOOST_INCLUDE% %LIBTORRENT_INCLUDE% swig\libtorrent_jni.cpp
