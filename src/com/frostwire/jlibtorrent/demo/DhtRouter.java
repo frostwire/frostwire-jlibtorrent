9d2c531c68e227ca134a61511a25a00a787458fb
Alden Torres <aldenml@gmail.com>
2015-01-10 10:34:03 -0500

    Missing file.

---
+++ src/com/frostwire/jlibtorrent/demo/DhtRouter.java
@@ -0,0 +1,43 @@
+package com.frostwire.jlibtorrent.demo;
+
+import com.frostwire.jlibtorrent.*;
+import com.frostwire.jlibtorrent.alerts.Alert;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author gubatron
+ * @author aldenml
+ */
+public final class DhtRouter {
+
+    public static void main(String[] args) throws Throwable {
+
+        Session s = new Session(new Fingerprint(), new Pair<Integer, Integer>(0, 0), "0.0.0.0", defaultRouters());
+        s.addListener(new AlertListener() {
+            @Override
+            public int[] types() {
+                return null;
+            }
+
+            @Override
+            public void alert(Alert<?> alert) {
+                System.out.println(alert.getType() + " - " + alert.getSwig().what() + " - " + alert.getSwig().message());
+            }
+        });
+        DHT dht = new DHT(s);
+
+        System.out.println("Waiting for nodes in DHT");
+        dht.waitNodes(1);
+        System.out.println("Nodes in DHT: " + dht.nodes());
+    }
+
+    private static List<Pair<String, Integer>> defaultRouters() {
+        List<Pair<String, Integer>> list = new LinkedList<Pair<String, Integer>>();
+
+        list.add(new Pair<String, Integer>("54.205.168.85", 6881));
+
+        return list;
+    }
+}
