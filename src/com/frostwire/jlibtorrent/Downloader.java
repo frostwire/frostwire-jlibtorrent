69611a6a8a2e5b6f3aec6b29ec28043ebaca1f4f
gubatron <gubatron@gmail.com>
2015-01-15 23:33:42 -0500

    fixes issue where it wouldn't add whole torrent to existing partial download.

--- src/com/frostwire/jlibtorrent/Downloader.java
+++ src/com/frostwire/jlibtorrent/Downloader.java
@@ -46,6 +46,11 @@ public final class Downloader {
 
                 th.prioritizeFiles(priorities);
                 s.fireAlert(new TorrentPrioritizeAlert(th));
+            } else {
+               // did they just add the entire torrent (therefore not selecting any priorities)
+                final Priority[] wholeTorrentPriorities = Priority.array(Priority.NORMAL, ti.getNumFiles());
+                th.prioritizeFiles(wholeTorrentPriorities);
+                s.fireAlert(new TorrentPrioritizeAlert(th));
             }
         } else { // new download
             s.asyncAddTorrent(ti, saveDir, priorities, resumeFile);
