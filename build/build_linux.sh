8c98ab8475305ce6d8730aeda307fe066d1f41be
Alden Torres <aldenml@gmail.com>
2015-02-27 08:03:05 -0800

    Fixed linux build script.

--- build/build_linux.sh
+++ build/build_linux.sh
@@ -12,9 +12,9 @@ JDK_INCLUDE_1=/usr/lib/jvm/java-7-openjdk-amd64/include
 JDK_INCLUDE_2=/usr/lib/jvm/java-7-openjdk-amd64/include/linux
 
 CXX=g++
-DEFINES="-DNDEBUG=1 -DBOOST_ASIO_SEPARATE_COMPILATION=1 -DTORRENT_USE_CLOCK_GETTIME=1 -DTORRENT_DISABLE_GEO_IP=1 -DTORRENT_NO_DEPRECATE=1"
+DEFINES="-DNDEBUG=1 -DBOOST_ASIO_SEPARATE_COMPILATION=1 -DTORRENT_USE_CLOCK_GETTIME=1 -DTORRENT_DISABLE_GEO_IP=1 -DTORRENT_NO_DEPRECATE=1 -DBOOST_ASIO_DISABLE_BOOST_CHRONO=1"
 INCLUDES="-I$BOOST_ROOT -I$LIBTORRENT_ROOT/include -I$JDK_INCLUDE_1 -I$JDK_INCLUDE_2"
-LIBS="-ltorrent -lboost_system -lboost_chrono -lboost_date_time -lboost_thread"
+LIBS="-ltorrent_trunk -lboost_system -lboost_chrono -lboost_date_time -lboost_thread"
 CXXFLAGS="-fPIC -fno-strict-aliasing -O3"
 LDFLAGS="-fPIC -Wl,-Bsymbolic -L$LIBTORRENT_LIBS"
 TARGET="libjlibtorrent.so"
