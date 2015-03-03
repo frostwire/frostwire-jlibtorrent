27815dc6d5059e349cd4731ac271963b3fd46872
Alden Torres <aldenml@gmail.com>
2015-03-03 12:04:30 -0500

    More on settings new API.

--- src/com/frostwire/jlibtorrent/SessionSettings.java
+++ src/com/frostwire/jlibtorrent/SessionSettings.java
@@ -1,6 +1,7 @@
 package com.frostwire.jlibtorrent;
 
 import com.frostwire.jlibtorrent.swig.session_settings;
+import com.frostwire.jlibtorrent.swig.settings_pack;
 
 /**
  * @author gubatron
@@ -16,5 +17,22 @@ public final class SessionSettings {
 
     public session_settings getSwig() {
         return s;
+    }
+
+
+    public int getDownloadRateLimit() {
+        return s.get_int(settings_pack.int_types.download_rate_limit.swigValue());
+    }
+
+    public int getUploadRateLimit() {
+        return s.get_int(settings_pack.int_types.upload_rate_limit.swigValue());
+    }
+
+    public int getMaxQueuedDiskBytes() {
+        return s.get_int(settings_pack.int_types.max_queued_disk_bytes.swigValue());
+    }
+
+    public int getSendBufferWatermark() {
+        return s.get_int(settings_pack.int_types.send_buffer_watermark.swigValue());
     }
 }
