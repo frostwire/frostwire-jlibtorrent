c55932995c4576e8d7fba83ec79ae826d8bda045
gubatron <gubatron@gmail.com>
2015-03-10 00:24:58 -0400

    More detailed instructions, build_linux.sh

--- build/build_linux.sh
+++ build/build_linux.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
-# Make sure these environment variables set.
-#$BOOST_ROOT
-#$LIBTORRENT_ROOT
+# Make sure these environment variables set and exported.
+# $BOOST_ROOT
+# $LIBTORRENT_ROOT
 
 # LIBTORRENT_LIBS is Ideally a folder where all the libtorrent .a files will be.
-#$LIBTORRENT_LIBS
+# $LIBTORRENT_LIBS
 
+# BUILDING BOOST
 # The first time you will need to build boost using the following 2 lines.
-#export CXXFLAGS="-std=c++11 -O3 -fPIC -I$BOOST_ROOT"
+# export CXXFLAGS="-std=c++11 -O3 -fPIC -I$BOOST_ROOT"
+
 # Download boost from boost.org (1.56 is the version we use, we've had high CPU usage issues with 1.57 and Windows Networking)
 # run the ./bootstrap.sh script so that ./b2 will work. Then execute:
-#$BOOST_ROOT/b2 variant=release link=static --stagedir=linux stage cxxflags=-fPIC cflags=-fPIC --with-chrono
+# $BOOST_ROOT/b2 variant=release link=static --stagedir=linux stage cxxflags=-fPIC cflags=-fPIC --with-chrono --with-system --with-random --with-thread
 
+# BUILDING libtorrent
 # After you've built boost, you should build libtorrent like this from your $LIBTORRENT_ROOT folder.
-#$BOOST_ROOT/bjam toolset=gcc variant=release link=static deprecated-functions=off boost=source cxxflags=-fPIC cflags=-fPIC
+# $BOOST_ROOT/bjam toolset=gcc variant=release link=static deprecated-functions=off boost=source cxxflags=-fPIC cflags=-fPIC
 
-# All the lines above we leave commented as they're only needed as requirements to build frostwire-jlibtorrent.
-# All further compilations will just need the script below.
+# Once you've build boost and libtorrent, copy all the .a files to $LIBTORRENT_LIBS wherever that may be.
+# You should end up with a list like follows:
+# 
+# ls -l $LIBTORRENT_LIBS
+# total 10128
+# -rw-rw-r-- 1 gubatron gubatron  115194 Mar 10 00:03 libboost_chrono.a
+# -rw-rw-r-- 1 gubatron gubatron   39954 Mar 10 00:03 libboost_random.a
+# -rw-rw-r-- 1 gubatron gubatron   22362 Mar 10 00:03 libboost_system.a
+# -rw-rw-r-- 1 gubatron gubatron  275180 Mar 10 00:03 libboost_thread.a
+# -rw-rw-r-- 1 gubatron gubatron 9904828 Mar 10 00:03 libtorrent.a
+
+# OTHER REQUIREMENTS
+# If you are in Ubuntu:
+# sudo apt-get install build-essentials libpcre3-dev
+#
+# Download swig 3 from swig.org (./configure; ./make; sudo make install)
 
 JDK_INCLUDE_1=/usr/lib/jvm/java-7-openjdk-amd64/include
 JDK_INCLUDE_2=/usr/lib/jvm/java-7-openjdk-amd64/include/linux
