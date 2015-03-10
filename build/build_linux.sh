8a5f888e09e965d516f07d2b6ac58dca4b611a1e
gubatron <gubatron@gmail.com>
2015-03-09 22:56:50 -0400

    more comments on linux build instructions

--- build/build_linux.sh
+++ build/build_linux.sh
@@ -3,16 +3,20 @@
 # Make sure these environment variables set.
 #$BOOST_ROOT
 #$LIBTORRENT_ROOT
+
+# LIBTORRENT_LIBS is Ideally a folder where all the libtorrent .a files will be.
 #$LIBTORRENT_LIBS
 
-# The first time you will need to build boost using the following 2 lines
+# The first time you will need to build boost using the following 2 lines.
 #export CXXFLAGS="-std=c++11 -O3 -fPIC -I$BOOST_ROOT"
-#$BOOST_ROOT/b2 variant=release link=static --stagedir=linux stage cxxflags=-fPIC cflags=-fPIC
+# Download boost from boost.org (1.56 is the version we use, we've had high CPU usage issues with 1.57 and Windows Networking)
+# run the ./bootstrap.sh script so that ./b2 will work. Then execute:
+#$BOOST_ROOT/b2 variant=release link=static --stagedir=linux stage cxxflags=-fPIC cflags=-fPIC --with-chrono
 
-# After you've built boost, you should build libtorrent like this
+# After you've built boost, you should build libtorrent like this from your $LIBTORRENT_ROOT folder.
 #$BOOST_ROOT/bjam toolset=gcc variant=release link=static deprecated-functions=off boost=source cxxflags=-fPIC cflags=-fPIC
 
-# All the lines above we leave commented as they're only needed as requirements to build frostwire-jlibtorrent
+# All the lines above we leave commented as they're only needed as requirements to build frostwire-jlibtorrent.
 # All further compilations will just need the script below.
 
 JDK_INCLUDE_1=/usr/lib/jvm/java-7-openjdk-amd64/include
