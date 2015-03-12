5dd6672f24e062936e76d4fe4dc8c81f2a4d5337
Alden Torres <aldenml@gmail.com>
2015-03-12 17:42:37 -0400

    Fixed compilations in android arm.

--- build/build_android.sh
+++ build/build_android.sh
@@ -8,14 +8,14 @@
 
 #copy user-config.jam to ~/ change it for intel if necessary
 #make-standalone-toolchain.sh --platform=android-15 --arch=arm --toolchain=arm-linux-androideabi-4.8 --install-dir=toolchain-platform-15-arm-gcc-4.8
-#ARM: export CXXFLAGS="-mthumb -fno-strict-aliasing -lstdc++ -O3 -D__GLIBC__ -D_GLIBCXX__PTHREADS -D__arm__ -D_REENTRANT -I$BOOST_ROOT"
+#ARM: export CXXFLAGS="-mthumb -fno-strict-aliasing -lstdc++ -O3 -D__GLIBC__ -D_GLIBCXX__PTHREADS -D__arm__ -D_REENTRANT -I$BOOST_ROOT -DBOOST_ATOMIC_LLONG_LOCK_FREE=2"
 #X86: export CXXFLAGS="-fno-strict-aliasing -lstdc++ -O3 -D__GLIBC__ -D_GLIBCXX__PTHREADS -D_REENTRANT -I$BOOST_ROOT"
 #$BOOST_ROOT/b2 toolset=gcc-arm variant=release link=static target-os=linux --stagedir="android-arm" stage
 #$BOOST_ROOT/bjam toolset=gcc-arm variant=release link=static target-os=linux deprecated-functions=off
 
 CXX=arm-linux-androideabi-g++
 CXXFLAGS="-mthumb -D__arm__ -fno-strict-aliasing -lstdc++ -O3 -D__GLIBC__ -D_GLIBCXX__PTHREADS -D_REENTRANT"
-DEFINES="-DNDEBUG=1 -DBOOST_ASIO_SEPARATE_COMPILATION=1 -DTORRENT_USE_CLOCK_GETTIME=1 -DTORRENT_DISABLE_GEO_IP=1 -DTORRENT_NO_DEPRECATE=1 -DBOOST_ASIO_DISABLE_STD_CHRONO=1 -DBOOST_ASIO_HAS_BOOST_CHRONO=1"
+DEFINES="-DNDEBUG=1 -DBOOST_ASIO_SEPARATE_COMPILATION=1 -DTORRENT_USE_CLOCK_GETTIME=1 -DTORRENT_DISABLE_GEO_IP=1 -DTORRENT_NO_DEPRECATE=1 -DBOOST_ASIO_DISABLE_STD_CHRONO=1 -DBOOST_ASIO_HAS_BOOST_CHRONO=1 -DBOOST_ATOMIC_LLONG_LOCK_FREE=2"
 INCLUDES="-I$BOOST_ROOT -I$LIBTORRENT_ROOT/include -I$JDK_INCLUDE_1 -I$JDK_INCLUDE_2"
 LIBS="-ltorrent_trunk -lboost_system -lboost_chrono -lboost_date_time -lboost_thread -lboost_atomic -lboost_random"
 LDFLAGS="-Wl,-Bsymbolic -L$LIBTORRENT_LIBS"
