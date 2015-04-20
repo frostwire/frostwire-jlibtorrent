2c821ba99f31153944cfc62a12fd1a77a5a0bafc
Alden Torres <aldenml@gmail.com>
2015-04-20 01:55:42 -0400

    Compiled Mac OS X binary with boost 1.58 + libtorrent trunk + lto flag.

--- build/build_macosx.sh
+++ build/build_macosx.sh
@@ -6,16 +6,16 @@
 #$JDK_INCLUDE_2
 #$LIBTORRENT_LIBS
 
-#export CXXFLAGS="-stdlib=libc++ -std=c++11 -O3 -I$BOOST_ROOT"
-#$BOOST_ROOT/b2 variant=release link=static --stagedir=macosx stage
-#$BOOST_ROOT/bjam toolset=darwin variant=release link=static deprecated-functions=off
+#export CXXFLAGS="-stdlib=libc++ -std=c++11 -O3 -flto -I$BOOST_ROOT"
+#$BOOST_ROOT/b2 variant=release link=static --stagedir=macosx stage --without-context --without-coroutine --without-python --without-mpi --without-wave --without-test --without-graph --without-graph_parallel --without-iostreams
+#$BOOST_ROOT/bjam toolset=darwin variant=release link=static deprecated-functions=off logging=off
 
 CXX=clang++
-CXXFLAGS="-stdlib=libc++ -std=c++11 -O3"
+CXXFLAGS="-stdlib=libc++ -std=c++11 -O3 -flto"
 DEFINES="-DNDEBUG=1 -DBOOST_ASIO_SEPARATE_COMPILATION=1 -DTORRENT_USE_ABSOLUTE_TIME=1 -DTORRENT_DISABLE_GEO_IP=1 -DTORRENT_NO_DEPRECATE=1"
-INCLUDES="-I$BOOST_ROOT -I$LIBTORRENT_ROOT/include -I$JDK_INCLUDE_1 -I$JDK_INCLUDE_2"
+INCLUDES="-I$BOOST_ROOT -I$LIBTORRENT_ROOT/include -I$LIBTORRENT_ROOT/include/libtorrent -I$JDK_INCLUDE_1 -I$JDK_INCLUDE_2"
 LIBS="-ltorrent -lboost_system -lboost_chrono -lboost_date_time -lboost_thread -lboost_random"
-LDFLAGS="-L$LIBTORRENT_LIBS"
+LDFLAGS="-flto -L$LIBTORRENT_LIBS"
 TARGET="libjlibtorrent.dylib"
 
 $CXX $CXXFLAGS $DEFINES $INCLUDES -c swig/libtorrent_jni.cpp
