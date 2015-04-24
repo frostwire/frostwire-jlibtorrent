d00b4f022ef1059309f5584f9500233547ab5461
Alden Torres <aldenml@gmail.com>
2015-04-24 12:07:20 -0400

    Cleanup and changes from master.

--- build/build_windows.bat
+++ build/build_windows.bat
@@ -5,32 +5,29 @@ rem ============================================================================
 rem DOWNLOAD BOOST BINARIES THAT MATCH YOUR VISUAL STUDIO VERSION
 rem =====================================================================================================================================================
 rem WARNING: Stay away from boost 1.56, 1.57, networking functions will cause a periodic freezing with libtorrent.
-rem You will need Visual Studio 2012 for its cl.exe compiler.
-rem Add the following folders to the %PATH% environment variable
-rem C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin (or similar depending on what you have in your system)
-rem C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE
-rem 1. Download boost_1_55_0-msvc-11.0-32.exe [http://sourceforge.net/projects/boost/files/boost-binaries/1.55.0-build2/boost_1_55_0-msvc-11.0-32.exe/download]
-rem 2. Extract it in "c:\boost_1_55_0"
+rem You will need Visual Studio 2013.
+rem Use the "VS2013 x86 Native Tools Command Prompt"
+rem 1. Download boost_1_58 source code
+rem 2. Extract it in "c:\boost_1_58_0"
 rem 2. invoke bootstrap.bat to have bjam working, you will need it to build libtorrent
 rem To build (1.58)
-rem %BOOST_ROOT%\b2 cxxflags="%CXXFLAGS%" variant=release link=static runtime-link=static --stagedir=windows stage --without-context --without-coroutine --without-python --without-mpi --without-wave --without-test --without-graph --without-graph_parallel --without-iostreams
+rem %BOOST_ROOT%\b2 cxxflags="/O2 /GL /DBOOST_ASIO_DISABLE_CONNECTEX=1" linkflags="/LTCG" variant=release link=static runtime-link=static --stagedir=windows stage --without-context --without-coroutine --without-python --without-mpi --without-wave --without-test --without-graph --without-graph_parallel --without-iostreams --without-math
 
 rem =====================================================================================================================================================
 rem BUILDING LIBTORRENT
 rem =====================================================================================================================================================
-rem Download a libtorrent subversion "trunk/" snapshot from http://sourceforge.net/p/libtorrent/code/HEAD/tree/trunk/
+rem Download a libtorrent subversion "braches/RC_1_0/" snapshot from http://sourceforge.net/p/libtorrent/code/HEAD/tree/trunk/
 rem set BOOST_ROOT=C:\boost_1_58_0
-rem set CXXFLAGS=to the actual value (see below)
-rem %BOOST_ROOT%\bjam toolset=msvc cxxflags="/I\"%BOOST_ROOT%\"" variant=release link=static runtime-link=static deprecated-functions=off logging=off
+rem %BOOST_ROOT%\bjam toolset=msvc cxxflags="/O2 /GL /DBOOST_ASIO_DISABLE_CONNECTEX=1" linkflags="/LTCG" variant=release link=static runtime-link=static deprecated-functions=off logging=none boost=source
 
 rem =====================================================================================================================================================
 rem BUILD OPTIONS AND VARIABLES (adjust paths depending on your build environment)
 rem =====================================================================================================================================================
 echo on
-set CXXFLAGS=/MT /O2 /fp:fast /nologo /DWIN32 /EHsc /c /GL /DBOOST_ASIO_DISABLE_CONNECTEX=1 /DNDEBUG=1 /DBOOST_ASIO_SEPARATE_COMPILATION=1 /DTORRENT_DISABLE_GEO_IP=1 /DTORRENT_NO_DEPRECATE=1 /DWIN32=1
+set CXXFLAGS=/MT /O2 /fp:fast /nologo /DWIN32 /EHsc /c /GL /DBOOST_ASIO_DISABLE_CONNECTEX=1 /DNDEBUG=1 /DBOOST_ASIO_SEPARATE_COMPILATION=1 /DTORRENT_DISABLE_GEO_IP=1 /DTORRENT_NO_DEPRECATE=1
 
 set BOOST_ROOT=C:\boost_1_58_0
-set LIBTORRENT_ROOT=C:\libtorrent-trunk
+set LIBTORRENT_ROOT=C:\libtorrent-RC_1_0
 set JAVA_HOME=C:\Program Files (x86)\Java\jdk1.8.0_25
 set VS_VERSION=12
 set VS_ROOT=C:\Program Files (x86)\Microsoft Visual Studio %VS_VERSION%.0
@@ -38,12 +35,12 @@ set VS_ROOT=C:\Program Files (x86)\Microsoft Visual Studio %VS_VERSION%.0
 set VS_INCLUDE=/I"%VS_ROOT%\VC\include"
 set BOOST_INCLUDE=/I"%BOOST_ROOT%"
 set JAVA_INCLUDES=/I"%JAVA_HOME%\include" /I"%JAVA_HOME%\include\win32" 
-set LIBTORRENT_INCLUDE=/I"%LIBTORRENT_ROOT%\include" /I"%LIBTORRENT_ROOT%\include\libtorrent"
+set LIBTORRENT_INCLUDE=/I"%LIBTORRENT_ROOT%\include"
 
 set INCLUDE_PATHS=%VS_INCLUDE% %BOOST_INCLUDE% %LIBTORRENT_INCLUDE% %JAVA_INCLUDES%
 
 set BOOST_LIBPATH=%BOOST_ROOT%\windows\lib
-set LIBTORRENT_LIBPATH=%LIBTORRENT_ROOT%\bin\msvc-%VS_VERSION%.0\release\deprecated-functions-off\link-static\logging-off\runtime-link-static\threading-multi
+set LIBTORRENT_LIBPATH=%LIBTORRENT_ROOT%\bin\msvc-%VS_VERSION%.0\release\boost-source\deprecated-functions-off\link-static\runtime-link-static\threading-multi
 set VS_LIBPATH=%VS_ROOT%\VC\lib
 
 @echo off
