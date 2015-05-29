d33f4bd9eac67511cf9285bfde2dac3e9073a104
Alden Torres <aldenml@gmail.com>
2015-05-29 15:18:14 -0400

    Fixed memory problems with SwigPeerPlugin.

--- src/com/frostwire/jlibtorrent/plugins/SwigTorrentPlugin.java
+++ src/com/frostwire/jlibtorrent/plugins/SwigTorrentPlugin.java
@@ -1,9 +1,10 @@
 package com.frostwire.jlibtorrent.plugins;
 
-import com.frostwire.jlibtorrent.TcpEndpoint;
-import com.frostwire.jlibtorrent.swig.swig_torrent_plugin;
-import com.frostwire.jlibtorrent.swig.tcp_endpoint;
-import com.frostwire.jlibtorrent.swig.torrent;
+import com.frostwire.jlibtorrent.*;
+import com.frostwire.jlibtorrent.swig.*;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author gubatron
@@ -14,35 +15,38 @@ public final class SwigTorrentPlugin extends swig_torrent_plugin {
     private final TorrentPlugin p;
     final torrent t;
 
+    private final List<SwigPeerPlugin> mem;
+
     public SwigTorrentPlugin(TorrentPlugin p, torrent t) {
         this.p = p;
         this.t = t;
+
+        this.mem = new LinkedList<SwigPeerPlugin>();
     }
 
-    /*
     @Override
     public swig_peer_plugin new_peer_connection(peer_connection pc) {
         PeerPlugin pp = p.newPeerConnection(new PeerConnection<peer_connection>(pc));
-        return pp != null ? new SwigPeerPlugin(pp) : new swig_peer_plugin();
+        return pp != null ? pin(new SwigPeerPlugin(pp)) : super.new_peer_connection(pc);
     }
 
     @Override
     public swig_peer_plugin new_bt_peer_connection(bt_peer_connection pc) {
         PeerPlugin pp = p.newPeerConnection(new BtPeerConnection(pc));
-        return pp != null ? new SwigPeerPlugin(pp) : new swig_peer_plugin();
+        return pp != null ? pin(new SwigPeerPlugin(pp)) : super.new_bt_peer_connection(pc);
     }
 
     @Override
     public swig_peer_plugin new_web_peer_connection(web_peer_connection pc) {
         PeerPlugin pp = p.newPeerConnection(new WebPeerConnection(pc));
-        return pp != null ? new SwigPeerPlugin(pp) : new swig_peer_plugin();
+        return pp != null ? pin(new SwigPeerPlugin(pp)) : super.new_web_peer_connection(pc);
     }
 
     @Override
     public swig_peer_plugin new_http_seed_connection(http_seed_connection pc) {
         PeerPlugin pp = p.newPeerConnection(new HttpSeedConnection(pc));
-        return pp != null ? new SwigPeerPlugin(pp) : new swig_peer_plugin();
-    }*/
+        return pp != null ? pin(new SwigPeerPlugin(pp)) : super.new_http_seed_connection(pc);
+    }
 
     @Override
     public void on_piece_pass(int index) {
@@ -92,5 +96,10 @@ public final class SwigTorrentPlugin extends swig_torrent_plugin {
     @Override
     public void on_add_peer(tcp_endpoint endp, int src, int flags) {
         p.onAddPeer(new TcpEndpoint(endp), src, flags);
+    }
+
+    private SwigPeerPlugin pin(SwigPeerPlugin p) {
+        mem.add(p);
+        return p;
     }
 }
