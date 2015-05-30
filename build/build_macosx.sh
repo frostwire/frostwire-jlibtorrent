8b61e815d5183e9281679ffb468f6d442350f422
Alden Torres <aldenml@gmail.com>
2015-05-30 19:16:48 -0400

    Fixed compilation script to use native apple clang (fixed my private environment). Updated mac os x binaries (compiled with openssl library).

--- build/build_macosx.sh
+++ build/build_macosx.sh
@@ -7,13 +7,13 @@
 #$LIBTORRENT_LIBS
 
 #$BOOST_ROOT/b2 cxxflags="-O2" variant=release link=static --stagedir=macosx stage --without-context --without-coroutine --without-python --without-mpi --without-wave --without-test --without-graph --without-graph_parallel --without-iostreams --without-math
-#$BOOST_ROOT/bjam toolset=darwin cxxflags="-O2" variant=release link=static deprecated-functions=off logging=none boost=source
+#$BOOST_ROOT/bjam toolset=darwin cxxflags="-O2" variant=release link=static deprecated-functions=off logging=none encryption=openssl boost=source
 
-CXX=g++
-CXXFLAGS="-std=c++11 -O2"
+CXX=clang++
+CXXFLAGS="-stdlib=libc++ -std=c++11 -O2"
 DEFINES="-DNDEBUG=1 -DBOOST_ASIO_SEPARATE_COMPILATION=1 -DTORRENT_USE_ABSOLUTE_TIME=1 -DTORRENT_DISABLE_GEO_IP=1 -DTORRENT_NO_DEPRECATE=1"
 INCLUDES="-I$BOOST_ROOT -I$LIBTORRENT_ROOT/include -I$JDK_INCLUDE_1 -I$JDK_INCLUDE_2"
-LIBS="-ltorrent -lboost_system -lboost_chrono -lboost_date_time -lboost_thread -lboost_random"
+LIBS="-ltorrent -lboost_system -lboost_chrono -lboost_date_time -lboost_thread -lboost_random -lssl -lcrypto"
 LDFLAGS="-L$LIBTORRENT_LIBS"
 TARGET="libjlibtorrent.dylib"
 
