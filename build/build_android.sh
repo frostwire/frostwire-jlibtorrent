2de31b84109886f74fbffccd3c9455b547fa73a9
Alden Torres <aldenml@gmail.com>
2015-06-02 10:04:39 -0400

    Fixed android build script to use openssl. Updated android arm binary.

--- build/build_android.sh
+++ build/build_android.sh
@@ -9,13 +9,13 @@
 #copy user-config.jam to home
 #make-standalone-toolchain.sh --platform=android-15 --arch=arm --toolchain=arm-linux-androideabi-4.9 --install-dir=toolchain-platform-15-arm-gcc-4.9
 #$BOOST_ROOT/b2 toolset=gcc-arm variant=release link=static target-os=linux --stagedir="android-arm" stage --without-context --without-coroutine --without-python --without-mpi --without-wave --without-test --without-graph --without-graph_parallel --without-iostreams --without-math
-#$BOOST_ROOT/bjam toolset=gcc-arm variant=release link=static target-os=linux deprecated-functions=off logging=none boost=source
+#$BOOST_ROOT/bjam toolset=gcc-arm variant=release link=static target-os=linux deprecated-functions=off logging=none encryption=openssl boost=source
 
 CXX=arm-linux-androideabi-g++
-CXXFLAGS="-mthumb -fno-strict-aliasing -lstdc++ -std=c++11 -O3 -march=armv7-a -DNDEBUG -D__GLIBC__ -D_GLIBCXX__PTHREADS -D__arm__ -D_REENTRANT"
-DEFINES="-DNDEBUG=1 -DBOOST_ASIO_SEPARATE_COMPILATION=1 -DTORRENT_USE_CLOCK_GETTIME=1 -DTORRENT_DISABLE_GEO_IP=1 -DTORRENT_NO_DEPRECATE=1 -DBOOST_ASIO_DISABLE_STD_CHRONO=1 -DBOOST_ASIO_HAS_BOOST_CHRONO=1"
+CXXFLAGS="-mthumb -fno-strict-aliasing -lstdc++ -std=c++11 -Os -march=armv7-a -DNDEBUG -D__GLIBC__ -D_GLIBCXX__PTHREADS -D__arm__ -D_REENTRANT -fpermissive"
+DEFINES="-DNDEBUG=1 -DBOOST_ASIO_SEPARATE_COMPILATION=1 -DTORRENT_USE_CLOCK_GETTIME=1 -DTORRENT_DISABLE_GEO_IP=1 -DTORRENT_NO_DEPRECATE=1 -DBOOST_ASIO_DISABLE_STD_CHRONO=1 -DBOOST_ASIO_HAS_BOOST_CHRONO=1 -DTORRENT_USE_OPENSSL=1"
 INCLUDES="-I$BOOST_ROOT -I$LIBTORRENT_ROOT/include -I$JDK_INCLUDE_1 -I$JDK_INCLUDE_2"
-LIBS="-ltorrent -lboost_system -lboost_chrono -lboost_date_time -lboost_thread -lboost_atomic -lboost_random"
+LIBS="-ltorrent -lboost_system -lboost_chrono -lboost_date_time -lboost_thread -lboost_atomic -lboost_random -lssl -lcrypto"
 LDFLAGS="-Wl,-Bsymbolic -march=armv7-a -L$LIBTORRENT_LIBS"
 TARGET="libjlibtorrent.so"
 
