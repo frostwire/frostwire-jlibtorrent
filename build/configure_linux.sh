1bc743a4481a5c29aa87f2ab6b2034f73c4b5919
gubatron <gubatron@gmail.com>
2015-06-04 12:56:04 -0400

    New environment_linux.sh common script. (untested)
    - environment_linux.sh defines commons environment variables and
      functions used by configure_linux.sh and build_linux.sh
    - Made sure to not put source folders ($BOOST_ROOT, $LIBTORRENT_ROOT)
      inside $LIBTORRENT_LIBS, that folder is only meant for binary files.
    - Source folders go inside "~/src/""
      e.g.
      - "~/src/boost_1_58_0"
      - "~/src/libtorrent_RC_1_0"
    - $LIBTORRENT_LIBS folder goes inside ~/libs/
      eg. "~/libs/LIBTORRENT_LIBS"

--- build/configure_linux.sh
+++ build/configure_linux.sh
@@ -1,35 +1,39 @@
-# Boost and libtorrent versions:
-LIBTORRENT_VERSION="RC_1_0"
-BOOST_VERSION="1_58_0"
-
+#!/bin/bash
+source ./build/environment_linux.sh
+sanityCheck
 
 # Some setup
 sudo apt-get install libbz2-dev libssl1.0.0 libcrypto++9 subversion
-mkdir $HOME/LIBTORRENT_LIBS
-export LIBTORRENT_LIBS="$HOME/LIBTORRENT_LIBS"
-#export CXXFLAGS="-stdlib=libc++ -std=c++11 -O3 -I$BOOST_ROOT"
 
-# Download Libtorrent and Boost, and export the home folders
-cd $LIBTORRENT_LIBS
-wget -O boost_$BOOST_VERSION.tar.bz2 http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_$BOOST_VERSION.tar.bz2/download
-tar -xvf boost_$BOOST_VERSION.tar.bz2
+# Download BOOST to BOOST_ROOT folder.
+mkdir -p $BOOST_ROOT
+pushd $BOOST_ROOT
+wget -O boost_$BOOST_UNDERSCORED_VERSION.tar.bz2 http://sourceforge.net/projects/boost/files/boost/$BOOST_DOTTED_VERSION/boost_$BOOST_UNDERSCORED_VERSION.tar.bz2/download
+tar -xvf boost_$BOOST_UNDERSCORED_VERSION.tar.bz2
+popd
+
+# Download libtorrent to LIBTORRENT_ROOT
+mkdir -p $LIBTORRENT_ROOT
+pushd $LIBTORRENT_ROOT
 svn checkout svn://svn.code.sf.net/p/libtorrent/code/branches/$LIBTORRENT_VERSION libtorrent-$LIBTORRENT_VERSION
+popd
 
-# Set the exports
-export LIBTORRENT_ROOT="$LIBTORRENT_LIBS/libtorrent-$LIBTORRENT_VERSION"
-export BOOST_ROOT="$LIBTORRENT_LIBS/boost_$BOOST_VERSION"
-
-# Build boost and copy the .a files
-cd $BOOST_ROOT
+# Build BOOST and copy the .a files to LIBTORRENT_LIBS
+pushd $BOOST_ROOT
 sh ./bootstrap.sh
 rm -rf bin*
 rm -rf linux
 $BOOST_ROOT/b2 variant=release link=static --stagedir=linux stage cxxflags="-O2 -fPIC" --with-date_time --with-chrono --with-system --with-random --with-thread
 cp linux/lib/*.a $LIBTORRENT_LIBS
+popd
 
 # Build and copy the libtorrent .a files
-cd $LIBTORRENT_ROOT
+pushd $LIBTORRENT_ROOT
 $BOOST_ROOT/bjam toolset=gcc cxxflags="-O2 -fPIC" cflags="-fPIC" variant=release link=static deprecated-functions=off logging=none encryption=openssl boost=source
-sudo cp bin/gcc-4.8/release/boost-source/deprecated-functions-off/encryption-openssl/link-static/threading-multi/libtorrent.a $LIBTORRENT_LIBS/
+cp bin/gcc-$GCC_VERSION/release/boost-source/deprecated-functions-off/encryption-openssl/link-static/threading-multi/libtorrent.a $LIBTORRENT_LIBS/
+popd
 
 # Run the build_linux script....
+echo "If there were no errors building BOOST and libtorrent proceed now to execute:"
+echo "./build/build_linux.sh"
+echo ""
