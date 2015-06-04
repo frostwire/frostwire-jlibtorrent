613fafd450a76bbb94351baa6ca296e36660902f
gubatron <gubatron@gmail.com>
2015-06-04 13:14:45 -0400

    path fix.

--- build/configure_linux.sh
+++ build/configure_linux.sh
@@ -7,14 +7,14 @@ sudo apt-get install libbz2-dev libssl1.0.0 libcrypto++9 subversion
 
 # Download BOOST to BOOST_ROOT folder.
 mkdir -p $BOOST_ROOT
-pushd $BOOST_ROOT
+pushd $BOOST_ROOT/..
 wget -O boost_$BOOST_UNDERSCORED_VERSION.tar.bz2 http://sourceforge.net/projects/boost/files/boost/$BOOST_DOTTED_VERSION/boost_$BOOST_UNDERSCORED_VERSION.tar.bz2/download
 tar -xvf boost_$BOOST_UNDERSCORED_VERSION.tar.bz2
 popd
 
 # Download libtorrent to LIBTORRENT_ROOT
 mkdir -p $LIBTORRENT_ROOT
-pushd $LIBTORRENT_ROOT
+pushd $LIBTORRENT_ROOT/..
 svn checkout svn://svn.code.sf.net/p/libtorrent/code/branches/$LIBTORRENT_VERSION libtorrent-$LIBTORRENT_VERSION
 popd
 
