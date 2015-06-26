35585ddf13881981632374fca47b0d37c34deb0f
Alden Torres <aldenml@gmail.com>
2015-06-26 15:23:03 -0400

    Refactor of native linux build. Removed native linux binary from repository.

--- swig/build-linux.sh
+++ swig/build-linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-$BOOST_ROOT/bjam toolset=gcc cxxflags=-fPIC cflags=-fPIC location=bin/linux
-TARGET=../binaries/linux/x86_64/libjlibtorrent.so
-cp bin/linux/libjlibtorrent.so.1.1.0 $TARGET
+$BOOST_ROOT/bjam toolset=gcc cxxflags=-fPIC cflags=-fPIC location=bin/linux/x86_64
+mv bin/linux/x86_64/libjlibtorrent.so.1.1.0 bin/linux/x86_64/libjlibtorrent.so
+TARGET=bin/linux/x86_64/libjlibtorrent.so
 strip --strip-unneeded -x $TARGET
 cp $TARGET ../
