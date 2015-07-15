37a15d4046704c102b6e083217747828c7be3d82
Alden Torres <aldenml@gmail.com>
2015-07-15 14:53:16 -0400

    Added simple bash script to run tools. Updated local maven repo.

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "usage: run.sh <toolname> args"
+    exit 1
+fi
+
+TOOL_NAME=$1
+shift
+TOOL_ARGS=$@
+
+TOOL_CLASS=""
+
+case "$TOOL_NAME" in
+  "announce") TOOL_CLASS="Announce";;
+  "getpeers") TOOL_CLASS="GetPeers";;
+  "ltversion") TOOL_CLASS="LTVersion";;
+  "mktorrent") TOOL_CLASS="MkTorrent";;
+  "readtorrent") TOOL_CLASS="ReadTorrent";;
+  * ) echo "Tool name not supported"; exit 1;;
+esac
+
+java \
+    -cp repo/com/frostwire/jlibtorrent/1.0/jlibtorrent-1.0.jar \
+    "com.frostwire.jlibtorrent.tools.$TOOL_CLASS" ${TOOL_ARGS}
