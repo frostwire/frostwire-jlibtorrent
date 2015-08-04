93c7081e728381d8b40de4cf3f0e4f410d7063c7
Alden Torres <aldenml@gmail.com>
2015-08-04 15:22:52 -0400

    Refactor of native native artifact. Removed native binary from repository.

--- swig/build-android.sh
+++ swig/build-android.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-$BOOST_ROOT/bjam --user-config=android-config.jam toolset=gcc-android.arm target-os=android
-TARGET=../binaries/android/armeabi-v7a/libjlibtorrent.so
-cp bin/gcc-android.arm/release/boost-source/crypto-openssl/deprecated-functions-off/target-os-android/libjlibtorrent.so.1.1.0 $TARGET
+$BOOST_ROOT/bjam --user-config=android-config.jam toolset=gcc-android.arm target-os=android location=bin/android/armeabi-v7a
+mv bin/android/armeabi-v7a/libjlibtorrent.so.1.1.0 bin/android/armeabi-v7a/libjlibtorrent.so
+TARGET=bin/android/armeabi-v7a/libjlibtorrent.so
 $NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/arm-linux-androideabi-strip --strip-unneeded -x $TARGET
 
-$BOOST_ROOT/bjam --user-config=android-config.jam toolset=gcc-android.x86 target-os=android
-TARGET=../binaries/android/x86/libjlibtorrent.so
-cp bin/gcc-android.x86/release/boost-source/crypto-openssl/deprecated-functions-off/target-os-android/libjlibtorrent.so.1.1.0 $TARGET
+$BOOST_ROOT/bjam --user-config=android-config.jam toolset=gcc-android.x86 target-os=android location=bin/android/x86
+mv bin/android/x86/libjlibtorrent.so.1.1.0 bin/android/x86/libjlibtorrent.so
+TARGET=bin/android/x86/libjlibtorrent.so
 $NDK_ROOT/toolchains/x86-4.9/prebuilt/darwin-x86_64/bin/i686-linux-android-strip --strip-unneeded -x $TARGET
