8bce1468161edb1a4b4c7d86ba20a74de63392e5
aldenml <aldenml@gmail.com>
2015-08-26 15:02:12 -0400

    Fixing windows build script (something changed with VS2015).

--- swig/build-windows.bat
+++ swig/build-windows.bat
@@ -1,3 +1,3 @@
-%BOOST_ROOT%\bjam toolset=msvc location=bin/windows/x86
+%BOOST_ROOT%\bjam toolset=msvc include="%OPENSSL_ROOT%\\include" location=bin/windows/x86
 set TARGET=bin\windows\x86\jlibtorrent.dll
 copy %TARGET% ..\
