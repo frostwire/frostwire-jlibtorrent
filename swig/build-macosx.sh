31563a190c289c29e75e39065799e02f61c24428
Alden Torres <aldenml@gmail.com>
2015-10-20 00:46:49 -0400

    Fixed compilation with Xcode 7 in Mac OS X.

--- swig/build-macosx.sh
+++ swig/build-macosx.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-$BOOST_ROOT/bjam toolset=darwin location=bin/macosx
-mv bin/macosx/libjlibtorrent.dylib.1.1.0 bin/macosx/libjlibtorrent.dylib
+# to compile under Xcode 7 and openssl being deprecated
+CXXFLAGS="-std=c++11 -stdlib=libc++ -I/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift-migrator/sdk/MacOSX.sdk/usr/include -Wno-deprecated-declarations -Wno-reserved-id-macro -mmacosx-version-min=10.7"
+
+$BOOST_ROOT/bjam toolset=darwin location=bin/macosx cxxflags="$CXXFLAGS"
 TARGET=bin/macosx/libjlibtorrent.dylib
 strip -S -x $TARGET
 cp $TARGET ../
