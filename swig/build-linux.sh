7a58571e2794f844cb6cccf38e23b969fb6071b6
Alden Torres <aldenml@gmail.com>
2015-11-12 08:32:48 -0500

    Cleanup.

--- swig/build-linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-$BOOST_ROOT/bjam toolset=gcc cxxflags=-fPIC cflags=-fPIC location=bin/linux/x86_64
-mv bin/linux/x86_64/libjlibtorrent.so.1.1.0 bin/linux/x86_64/libjlibtorrent.so
-TARGET=bin/linux/x86_64/libjlibtorrent.so
-strip --strip-unneeded -x $TARGET
-cp $TARGET ../
--- swig/build-windows.bat
+++
@@ -1,3 +0,0 @@
-%BOOST_ROOT%\bjam toolset=msvc include="%OPENSSL_ROOT%\\include" location=bin/windows/x86
-set TARGET=bin\windows\x86\jlibtorrent.dll
-copy %TARGET% ..\
