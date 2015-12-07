227d3696c19b44fd97f0901bc5b4aca355f5df26
Alden Torres <aldenml@gmail.com>
2015-12-07 14:44:42 -0500

    Better implementation of GetMagnet demo.

--- src/test/java/com/frostwire/jlibtorrent/demo/GetMagnet.java
+++ src/test/java/com/frostwire/jlibtorrent/demo/GetMagnet.java
@@ -3,10 +3,8 @@ package com.frostwire.jlibtorrent.demo;
 import com.frostwire.jlibtorrent.*;
 import com.frostwire.jlibtorrent.alerts.Alert;
 import com.frostwire.jlibtorrent.alerts.AlertType;
-import com.frostwire.jlibtorrent.alerts.SessionStatsAlert;
-import com.frostwire.jlibtorrent.swig.counters;
+import com.frostwire.jlibtorrent.alerts.DhtStatsAlert;
 
-import java.io.File;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -21,41 +19,61 @@ public final class GetMagnet {
         //String uri = "magnet:?xt=urn:btih:86d0502ead28e495c9e67665340f72aa72fe304e&dn=Frostwire.5.3.6.+%5BWindows%5D&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.istole.it%3A6969&tr=udp%3A%2F%2Fopen.demonii.com%3A1337";
         String uri = "magnet:?xt=urn:btih:a83cc13bf4a07e85b938dcf06aa707955687ca7c";
 
-        Session s = new Session();
+        final Session s = new Session();
 
         final CountDownLatch signal = new CountDownLatch(1);
 
-        s.addListener(new AlertListener() {
+        // the session stats are posted about once per second.
+        AlertListener l = new AlertListener() {
             @Override
             public int[] types() {
-                return new int[]{AlertType.SESSION_STATS.getSwig()};
+                return new int[]{AlertType.SESSION_STATS.getSwig(), AlertType.DHT_STATS.getSwig()};
             }
 
             @Override
             public void alert(Alert<?> alert) {
-                System.out.println(alert);
                 if (alert.getType().equals(AlertType.SESSION_STATS)) {
-                    SessionStatsAlert stats = (SessionStatsAlert) alert;
+                    s.postDHTStats();
+                }
 
-                    System.out.println(stats.value(counters.stats_gauge_t.dht_immutable_data.swigValue()));
-                    long dhtPeers = 0;//stats.dhtPeers();
-                    System.out.println("Num peers: " + dhtPeers);
-                    // wait for at least 10 peers in our DHT storage.
-                    if (dhtPeers >= 10) {
+                if (alert.getType().equals(AlertType.DHT_STATS)) {
+
+                    long nodes = countNodes((DhtStatsAlert) alert);
+                    // wait for at least 10 nodes in the DHT.
+                    if (nodes >= 10) {
+                        System.out.println("DHT contains " + nodes + " nodes");
                         signal.countDown();
                     }
                 }
             }
-        });
+
+            private int countNodes(DhtStatsAlert a) {
+                DHTRoutingBucket[] t = a.getRoutingTable();
+
+                int total = 0;
+                for (int i = 0; i < t.length; i++) {
+                    total += t[i].numNodes();
+                }
+
+                return total;
+            }
+        };
+
+        s.addListener(l);
+        s.postDHTStats();
 
         Downloader d = new Downloader(s);
-        DHT dht = new DHT(s);
-
-        // triggers the internal peers discovery and population
-        dht.getPeers("a83cc13bf4a07e85b938dcf06aa707955687ca7c");
 
         System.out.println("Waiting for nodes in DHT (10 seconds)...");
-        signal.await(10, TimeUnit.SECONDS);
+        boolean r = signal.await(10, TimeUnit.SECONDS);
+        if (!r) {
+            System.out.println("DHT bootstrap timeout");
+            System.exit(0);
+        }
+
+        // no more trigger of DHT stats
+        s.removeListener(l);
+
 
         System.out.println("Fetching the magnet uri, please wait...");
         byte[] data = d.fetchMagnet(uri, 30000);
