b28576a66d68af238702af867d0a49eba4fc3542
gubatron <gubatron@gmail.com>
2015-12-31 17:21:40 -0500

    fix broken import path in test.

--- swig/session_test.js
+++ swig/session_test.js
@@ -1,10 +1,9 @@
-var lt = require('build/Release/jlibtorrent');
+var lt = require('./build/Release/jlibtorrent');
 
 var alert_mask = lt.alert.all_categories;
 var sp = new lt.settings_pack();
 
 sp.set_int(lt.settings_pack.alert_mask, alert_mask);
-
 var s = new lt.session(sp);
 
 lt.session_alerts_loop(s);
