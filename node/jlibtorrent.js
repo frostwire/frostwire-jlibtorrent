4a7eb6d378eb684e758dd450a28da3f119af2378
Alden Torres <aldenml@gmail.com>
2016-01-28 16:29:51 -0500

    [node] session_test using new API.

--- node/jlibtorrent.js
+++ node/jlibtorrent.js
@@ -89,3 +89,61 @@ var swig = require('./jlibtorrent.node');
     exports.SettingsPack = SettingsPack;
 
 }());
+
+// Session
+(function () {
+
+    function createSession(settings, logging) {
+        var sp = settings.swig(),
+            alert_mask = swig.alert.all_categories;
+        if (!logging) {
+            var log_mask = swig.alert.session_log_notification |
+                swig.alert.torrent_log_notification |
+                swig.alert.peer_log_notification |
+                swig.alert.dht_log_notification |
+                swig.alert.port_mapping_log_notification;
+            alert_mask = alert_mask & ~log_mask;
+        }
+
+        // we always override alert_mask since we use it for our internal operations
+        sp.set_int(swig.settings_pack.alert_mask, alert_mask);
+
+        return new swig.session(sp);
+    }
+
+    function alertsLoop(s) {
+        var session_alerts_loop = function () {
+            var max_wait = swig.to_milliseconds(100);
+            var alert = s.wait_for_alert(max_wait);
+
+            if (alert != null) {
+                var vector = new swig.alert_ptr_vector();
+                s.pop_alerts(vector);
+                var size = vector.size();
+                for (var i = 0; i < size; i++) {
+                    var a = vector.get(i);
+                    console.log(a.type() + " - " + a.what() + " - " + a.message());
+                }
+                vector.clear();
+            }
+        }
+
+        setInterval(session_alerts_loop, 1000);
+    }
+
+    function Session(settings, logging) {
+        settings = settings || new exports.SettingsPack();
+        logging = logging || false;
+
+        this.s = createSession(settings, logging);
+
+        alertsLoop(this.s);
+    }
+
+    Session.prototype.swig = function () {
+        return this.sp;
+    }
+
+    exports.Session = Session;
+
+}());
--- node/session_test.js
+++ node/session_test.js
@@ -1,37 +1,9 @@
-/*var lt = require('./jlibtorrent');
-
- var alert_mask = lt.alert.all_categories;
- var sp = new lt.settings_pack();
-
- sp.set_int(lt.settings_pack.alert_mask, alert_mask);
- var s = new lt.session(sp);
-
- function session_alerts_loop() {
- var max_wait = lt.to_milliseconds(100);
- var alert = s.wait_for_alert(max_wait);
-
- if (alert != null) {
- var vector = new lt.alert_ptr_vector();
- s.pop_alerts(vector);
- var size = vector.size();
- for (var i = 0; i < size; i++) {
- var a = vector.get(i);
- console.log(a.type() + " - " + a.what() + " - " + a.message());
- }
- vector.clear();
- }
- }
-
- setInterval(session_alerts_loop, 500);
- */
 var jlibtorrent = require('./jlibtorrent.js');
 
 console.log("Using libtorrent version: " + jlibtorrent.LibTorrent.fullVersion());
 
 var sp = new jlibtorrent.SettingsPack();
-var name = jlibtorrent.swig.settings_pack.peer_fingerprint;
-sp.string(name, "test");
-console.log(sp.string(name));
+var s = new jlibtorrent.Session(sp, false);
 
 process.stdout.write('Press ENTER to exit...');
 process.stdin.once('data', function (data) {
