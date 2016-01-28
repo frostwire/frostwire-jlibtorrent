81c214cc9701541d47f5409925453b8f6e333484
Alden Torres <aldenml@gmail.com>
2016-01-28 14:27:46 -0500

    Added node download_torrent test.

--- node/download_torrent.js
+++ node/download_torrent.js
@@ -1,6 +1,16 @@
 var lt = require('./jlibtorrent');
 
+var logging = false;
 var alert_mask = lt.alert.all_categories;
+if (!logging) {
+    var log_mask = lt.alert.session_log_notification |
+        lt.alert.torrent_log_notification |
+        lt.alert.peer_log_notification |
+        lt.alert.dht_log_notification |
+        lt.alert.port_mapping_log_notification;
+    alert_mask = alert_mask & ~log_mask;
+}
+
 var sp = new lt.settings_pack();
 
 sp.set_int(lt.settings_pack.alert_mask, alert_mask);
@@ -23,6 +33,14 @@ function session_alerts_loop() {
 }
 
 setInterval(session_alerts_loop, 500);
+
+var torrent = "/Users/aldenml/Downloads/LukHash_The_Other_Side_FrostClick_FrostWire_MP3_January_17_2016.torrent";
+
+var p = lt.add_torrent_params.create_instance();
+var ec = new lt.error_code();
+var ti = new lt.torrent_info(torrent, ec);
+p.set_ti(ti);
+s.async_add_torrent(p);
 
 process.stdout.write('Press ENTER to exit...');
 process.stdin.once('data', function (data) {
