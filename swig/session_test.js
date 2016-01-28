1752ac4558284776abb7c0cb1f7aabd4691c9a37
Alden Torres <aldenml@gmail.com>
2016-01-28 13:52:15 -0500

    Success with simple setTimeout solution for polling alerts.

--- swig/session_test.js
+++ swig/session_test.js
@@ -6,7 +6,23 @@ var sp = new lt.settings_pack();
 sp.set_int(lt.settings_pack.alert_mask, alert_mask);
 var s = new lt.session(sp);
 
-lt.session_alerts_loop(s);
+function session_alerts_loop() {
+    var max_wait = lt.to_milliseconds(100);
+    var alert = s.wait_for_alert(max_wait);
+
+    if (alert != null) {
+        var vector = new lt.alert_ptr_vector();
+        s.pop_alerts(vector);
+        var size = vector.size();
+        for (var i = 0; i < size; i++) {
+            var a = vector.get(i);
+            console.log(a.type() + " - " + a.what() + " - " + a.message());
+        }
+        vector.clear();
+    }
+}
+
+setTimeout(session_alerts_loop, 500);
 
 process.stdout.write('Press ENTER to exit...');
 process.stdin.once('data', function (data) {
