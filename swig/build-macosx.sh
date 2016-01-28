af4b19d4fe310b83bbb2300753d8e16a32b21c72
Alden Torres <aldenml@gmail.com>
2016-01-28 14:02:57 -0500

    Instrumenting build of nodejs native modile (only for Mac OS X).

--- swig/build-macosx.sh
+++ swig/build-macosx.sh
@@ -4,3 +4,6 @@ $BOOST_ROOT/b2 --user-config=config/macosx-native-x86_64-config.jam toolset=darw
 TARGET=bin/macosx/x86_64/libjlibtorrent.dylib
 strip -S -x $TARGET
 cp $TARGET ../
+
+node-gyp configure build
+cp build/Release/jlibtorrent.node ../node
