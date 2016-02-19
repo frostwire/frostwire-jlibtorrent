25c160b3a20f0e71d58997b18ff7d43ae7054dd5
Alden Torres <aldenml@gmail.com>
2016-02-19 12:12:33 -0500

    Implemented testAnnouncePeer

--- src/test/java/com/frostwire/jlibtorrent/DhtStorageBaseTest.java
+++ src/test/java/com/frostwire/jlibtorrent/DhtStorageBaseTest.java
@@ -5,6 +5,7 @@ import com.frostwire.jlibtorrent.swig.entry;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 /**
  * @author gubatron
@@ -13,6 +14,9 @@ import static org.junit.Assert.assertEquals;
 public class DhtStorageBaseTest {
 
     private static final Sha1Hash n1 = new Sha1Hash("5fbfbff10c5d6a4ec8a88e4c6ab4c28b95eee401");
+    private static final Sha1Hash n2 = new Sha1Hash("5fbfbff10c5d6a4ec8a88e4c6ab4c28b95eee402");
+    private static final Sha1Hash n3 = new Sha1Hash("5fbfbff10c5d6a4ec8a88e4c6ab4c28b95eee403");
+    private static final Sha1Hash n4 = new Sha1Hash("5fbfbff10c5d6a4ec8a88e4c6ab4c28b95eee404");
 
     @Test
     public void testAnnouncePeer() {
@@ -24,23 +28,22 @@ public class DhtStorageBaseTest {
 
         assertEquals(peers.get("n").type(), entry.data_type.undefined_t);
         assertEquals(peers.get("values").type(), entry.data_type.undefined_t);
-/*
-    tcp::endpoint p1 = ep("124.31.75.21", 1);
-	tcp::endpoint p2 = ep("124.31.75.22", 1);
-	tcp::endpoint p3 = ep("124.31.75.23", 1);
-	tcp::endpoint p4 = ep("124.31.75.24", 1);
 
-	s->announce_peer(n1, p1, "torrent_name", false);
-	s->get_peers(n1, false, false, peers);
-	TEST_EQUAL(peers["n"].string(), "torrent_name")
-	TEST_EQUAL(peers["values"].list().size(), 1)
+        TcpEndpoint p1 = new TcpEndpoint("124.31.75.21", 1);
+        TcpEndpoint p2 = new TcpEndpoint("124.31.75.22", 1);
+        TcpEndpoint p3 = new TcpEndpoint("124.31.75.23", 1);
+        TcpEndpoint p4 = new TcpEndpoint("124.31.75.24", 1);
 
-	s->announce_peer(n2, p2, "torrent_name1", false);
-	s->announce_peer(n2, p3, "torrent_name1", false);
-	s->announce_peer(n3, p4, "torrent_name2", false);
-	bool r = s->get_peers(n1, false, false, peers);
-	TEST_CHECK(!r);
-         */
+        s.announcePeer(n1, p1, "torrent_name", false);
+        s.getPeers(n1, false, false, peers);
+        assertEquals(peers.get("n").string(), "torrent_name");
+        assertEquals(peers.get("values").list().size(), 1);
+
+        s.announcePeer(n2, p2, "torrent_name1", false);
+        s.announcePeer(n2, p3, "torrent_name1", false);
+        s.announcePeer(n3, p4, "torrent_name2", false);
+        boolean r = s.getPeers(n1, false, false, peers);
+        assertFalse(r);
     }
 
     private static DhtSettings testSettings() {
