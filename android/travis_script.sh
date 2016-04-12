7dfef8b336ee43668b482aec5ab4b7574634e00e
Alden Torres <aldenml@gmail.com>
2016-04-12 01:36:40 -0400

    Download of NDK and boost in travis

--- android/travis_script.sh
+++ android/travis_script.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+wget -O android-ndk.zip http://dl.google.com/android/repository/android-ndk-r11c-linux-x86_64.zip
+echo "Extracting NDK...wait"
+unzip -qq android-ndk.zip
+export NDK_ROOT=$PWD/android-ndk
+
 echo no | android create avd --force --name test --target $android_target --abi $android_abi
 emulator -avd test -no-skin -no-audio -no-window &
+
 gradle check -PdisablePreDex --continue --stacktrace
 android-wait-for-emulator
 adb devices
