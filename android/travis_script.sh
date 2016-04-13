af438fd01a89e5cf5aa9e0a099350c0b9f90e862
Alden Torres <aldenml@gmail.com>
2016-04-13 13:07:36 -0400

    Running android unit tests in emulator

--- android/travis_script.sh
+++ android/travis_script.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-wget -O android-ndk.zip http://dl.google.com/android/repository/android-ndk-r11c-linux-x86_64.zip
-echo "Extracting NDK...wait"
-unzip -qq android-ndk.zip
-export NDK_ROOT=$PWD/android-ndk
-
-echo no | android create avd --force --name test --target $android_target --abi $android_abi
+echo no | android create avd --force --name test --target android-23 --abi $android_abi
 emulator -avd test -no-skin -no-audio -no-window &
 
 gradle check -PdisablePreDex --continue --stacktrace
