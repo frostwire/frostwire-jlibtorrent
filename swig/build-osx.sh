083ad17bb4c61d9d86064f197c6bb7a09d60ea94
Alden Torres <aldenml@gmail.com>
2016-04-13 18:01:45 -0400

    Added build script for local development in native osx (not travis)

---
+++ swig/build-osx.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+export DEVELOPMENT_ROOT=~/Development
+
+if [ -z ${BOOST_ROOT+x} ]; then
+    export BOOST_ROOT=$DEVELOPMENT_ROOT/boost_1_60_0
+fi
+
+if [ -z ${LIBTORRENT_ROOT+x} ]; then
+    export LIBTORRENT_ROOT=$DEVELOPMENT_ROOT/libtorrent
+fi
+
+$BOOST_ROOT/b2 --user-config=config/macosx-x86_64-config.jam \
+            toolset=darwin-x86_64 target-os=darwin location=bin/macosx/x86_64 \
+            cxxflags="-I/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift-migrator/sdk/MacOSX.sdk/usr/include"
+cp bin/macosx/x86_64/libjlibtorrent.dylib ../
+
+node-gyp configure build
+cp build/Release/jlibtorrent.node ../node
