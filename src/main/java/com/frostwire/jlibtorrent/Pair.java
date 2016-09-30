fec6b66a5f1ba066fd6077c53678569c6598b442
Alden Torres <aldenml@gmail.com>
2016-09-30 17:20:12 -0400

    Pair documentation

--- src/main/java/com/frostwire/jlibtorrent/Pair.java
+++ src/main/java/com/frostwire/jlibtorrent/Pair.java
@@ -4,28 +4,34 @@ import com.frostwire.jlibtorrent.swig.string_int_pair;
 import com.frostwire.jlibtorrent.swig.string_string_pair;
 
 /**
+ * Utility function to mirror the C++ std::pair class
+ *
  * @author gubatron
  * @author aldenml
  */
 public final class Pair<T1, T2> {
 
+    /**
+     * @param first  first element
+     * @param second second element
+     */
     public Pair(T1 first, T2 second) {
         this.first = first;
         this.second = second;
     }
 
     /**
-     *
+     * the first element
      */
     public final T1 first;
 
     /**
-     *
+     * the second element
      */
     public final T2 second;
 
     /**
-     * @return
+     * @return a native object
      */
     string_string_pair to_string_string_pair() {
         if (!String.class.equals(first.getClass()) || !String.class.equals(second.getClass())) {
@@ -36,7 +42,7 @@ public final class Pair<T1, T2> {
     }
 
     /**
-     * @return
+     * @return a native object
      */
     string_int_pair to_string_int_pair() {
         if (!String.class.equals(first.getClass()) || !Integer.class.equals(second.getClass())) {
