864eeebe300aa52e782b37caf4ec4ef5ef7512ab
Alden Torres <aldenml@gmail.com>
2016-10-04 12:34:59 -0400

    some fixes for the debug info

--- swig/get-binaries.sh
+++ swig/get-binaries.sh
@@ -3,25 +3,30 @@
 rm -rf bin
 mkdir bin
 
+# android arm
 wget -P bin/release/android/armeabi-v7a https://s3.amazonaws.com/jlibtorrent1/release/android/armeabi-v7a/libjlibtorrent.so
 wget -P bin/debug/android/armeabi-v7a https://s3.amazonaws.com/jlibtorrent1/debug/android/armeabi-v7a/libjlibtorrent.so
+wget -P bin/debug/android/armeabi-v7a https://s3.amazonaws.com/jlibtorrent1/debug/android/armeabi-v7a/libjlibtorrent.so.debug
+# android
 wget -P bin/release/android/arm64-v8a https://s3.amazonaws.com/jlibtorrent1/release/android/arm64-v8a/libjlibtorrent.so
-wget -P bin/debug/android/arm64-v8a https://s3.amazonaws.com/jlibtorrent1/debug/android/arm64-v8a/libjlibtorrent.so
 wget -P bin/release/android/x86 https://s3.amazonaws.com/jlibtorrent1/release/android/x86/libjlibtorrent.so
-wget -P bin/debug/android/x86 https://s3.amazonaws.com/jlibtorrent1/debug/android/x86/libjlibtorrent.so
 wget -P bin/release/android/x86_64 https://s3.amazonaws.com/jlibtorrent1/release/android/x86_64/libjlibtorrent.so
-wget -P bin/debug/android/x86_64 https://s3.amazonaws.com/jlibtorrent1/debug/android/x86_64/libjlibtorrent.so
+
+# linux x86
 wget -P bin/release/linux/x86 https://s3.amazonaws.com/jlibtorrent1/release/linux/x86/libjlibtorrent.so
-wget -P bin/debug/linux/x86 https://s3.amazonaws.com/jlibtorrent1/debug/linux/x86/libjlibtorrent.so
+# linux x86_64
 wget -P bin/release/linux/x86_64 https://s3.amazonaws.com/jlibtorrent1/release/linux/x86_64/libjlibtorrent.so
 wget -P bin/debug/linux/x86_64 https://s3.amazonaws.com/jlibtorrent1/debug/linux/x86_64/libjlibtorrent.so
+wget -P bin/debug/linux/x86_64 https://s3.amazonaws.com/jlibtorrent1/debug/linux/x86_64/libjlibtorrent.so.debug
+
+# linux
 wget -P bin/release/linux/arm https://s3.amazonaws.com/jlibtorrent1/release/linux/arm/libjlibtorrent.so
-wget -P bin/debug/linux/arm https://s3.amazonaws.com/jlibtorrent1/debug/linux/arm/libjlibtorrent.so
 wget -P bin/release/linux/arm64 https://s3.amazonaws.com/jlibtorrent1/release/linux/arm64/libjlibtorrent.so
-wget -P bin/debug/linux/arm64 https://s3.amazonaws.com/jlibtorrent1/debug/linux/arm64/libjlibtorrent.so
+
+# windows
 wget -P bin/release/windows/x86 https://s3.amazonaws.com/jlibtorrent1/release/windows/x86/jlibtorrent.dll
-wget -P bin/debug/windows/x86 https://s3.amazonaws.com/jlibtorrent1/debug/windows/x86/jlibtorrent.dll
 wget -P bin/release/windows/x86_64 https://s3.amazonaws.com/jlibtorrent1/release/windows/x86_64/jlibtorrent.dll
-wget -P bin/debug/windows/x86_64 https://s3.amazonaws.com/jlibtorrent1/debug/windows/x86_64/jlibtorrent.dll
+
+# macos
 wget -P bin/release/macosx/x86_64 https://s3.amazonaws.com/jlibtorrent1/release/macosx/x86_64/libjlibtorrent.dylib
 wget -P bin/debug/macosx/x86_64 https://s3.amazonaws.com/jlibtorrent1/debug/macosx/x86_64/libjlibtorrent.dylib
