3753054383d8fee7b8d83b219bb5109b0d29882b
Alden Torres <aldenml@gmail.com>
2016-10-04 13:13:43 -0400

    dont compile macOS debug during development

--- swig/build-macos.sh
+++ swig/build-macos.sh
@@ -6,7 +6,6 @@ export LIBTORRENT_ROOT=$DEVELOPMENT_ROOT/libtorrent
 export OPENSSL_ROOT=$DEVELOPMENT_ROOT/openssl-macos
 
 $BOOST_ROOT/b2 --user-config=config/macosx-x86_64-config.jam variant=release toolset=clang-x86_64 target-os=darwin location=bin/release/macosx/x86_64
-$BOOST_ROOT/b2 --user-config=config/macosx-x86_64-config.jam variant=debug toolset=clang-x86_64 target-os=darwin location=bin/debug/macosx/x86_64
 cp bin/release/macosx/x86_64/libjlibtorrent.dylib ../
 
 node-gyp configure build
