d67f06e9f8e3157a2d2ae9992b225681fa1d3cd8
Alden Torres <aldenml@gmail.com>
2016-10-24 09:42:26 -0400

    get-binaries script cleanup

--- swig/get-binaries.sh
+++ swig/get-binaries.sh
@@ -5,8 +5,6 @@ mkdir bin
 
 # android arm
 wget -P bin/release/android/armeabi-v7a https://s3.amazonaws.com/jlibtorrent1/release/android/armeabi-v7a/libjlibtorrent.so
-wget -P bin/debug/android/armeabi-v7a https://s3.amazonaws.com/jlibtorrent1/debug/android/armeabi-v7a/libjlibtorrent.so
-wget -P bin/debug/android/armeabi-v7a https://s3.amazonaws.com/jlibtorrent1/debug/android/armeabi-v7a/libjlibtorrent.so.debug
 # android
 wget -P bin/release/android/arm64-v8a https://s3.amazonaws.com/jlibtorrent1/release/android/arm64-v8a/libjlibtorrent.so
 wget -P bin/release/android/x86 https://s3.amazonaws.com/jlibtorrent1/release/android/x86/libjlibtorrent.so
@@ -16,8 +14,6 @@ wget -P bin/release/android/x86_64 https://s3.amazonaws.com/jlibtorrent1/release
 wget -P bin/release/linux/x86 https://s3.amazonaws.com/jlibtorrent1/release/linux/x86/libjlibtorrent.so
 # linux x86_64
 wget -P bin/release/linux/x86_64 https://s3.amazonaws.com/jlibtorrent1/release/linux/x86_64/libjlibtorrent.so
-wget -P bin/debug/linux/x86_64 https://s3.amazonaws.com/jlibtorrent1/debug/linux/x86_64/libjlibtorrent.so
-wget -P bin/debug/linux/x86_64 https://s3.amazonaws.com/jlibtorrent1/debug/linux/x86_64/libjlibtorrent.so.debug
 
 # linux
 wget -P bin/release/linux/arm https://s3.amazonaws.com/jlibtorrent1/release/linux/arm/libjlibtorrent.so
@@ -29,5 +25,3 @@ wget -P bin/release/windows/x86_64 https://s3.amazonaws.com/jlibtorrent1/release
 
 # macos
 wget -P bin/release/macosx/x86_64 https://s3.amazonaws.com/jlibtorrent1/release/macosx/x86_64/libjlibtorrent.dylib
-wget -P bin/debug/macosx/x86_64 https://s3.amazonaws.com/jlibtorrent1/debug/macosx/x86_64/libjlibtorrent.dylib
-wget -P bin/debug/macosx/x86_64 https://s3.amazonaws.com/jlibtorrent1/debug/macosx/x86_64/libjlibtorrent.dylib.debug
