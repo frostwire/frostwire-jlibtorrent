8a404b80bfdd070e06a128073f9c3ea54e2681e6
Alden Torres <aldenml@gmail.com>
2016-11-14 17:27:01 -0500

    removed linux arm build and .travis cleanup

--- swig/get-binaries.sh
+++ swig/get-binaries.sh
@@ -3,21 +3,15 @@
 rm -rf bin
 mkdir bin
 
-# android arm
-wget -P bin/release/android/armeabi-v7a https://s3.amazonaws.com/jlibtorrent1/release/android/armeabi-v7a/libjlibtorrent.so
 # android
+wget -P bin/release/android/armeabi-v7a https://s3.amazonaws.com/jlibtorrent1/release/android/armeabi-v7a/libjlibtorrent.so
 wget -P bin/release/android/arm64-v8a https://s3.amazonaws.com/jlibtorrent1/release/android/arm64-v8a/libjlibtorrent.so
 wget -P bin/release/android/x86 https://s3.amazonaws.com/jlibtorrent1/release/android/x86/libjlibtorrent.so
 wget -P bin/release/android/x86_64 https://s3.amazonaws.com/jlibtorrent1/release/android/x86_64/libjlibtorrent.so
 
-# linux x86
-wget -P bin/release/linux/x86 https://s3.amazonaws.com/jlibtorrent1/release/linux/x86/libjlibtorrent.so
-# linux x86_64
-wget -P bin/release/linux/x86_64 https://s3.amazonaws.com/jlibtorrent1/release/linux/x86_64/libjlibtorrent.so
-
 # linux
-wget -P bin/release/linux/arm https://s3.amazonaws.com/jlibtorrent1/release/linux/arm/libjlibtorrent.so
-wget -P bin/release/linux/arm64 https://s3.amazonaws.com/jlibtorrent1/release/linux/arm64/libjlibtorrent.so
+wget -P bin/release/linux/x86 https://s3.amazonaws.com/jlibtorrent1/release/linux/x86/libjlibtorrent.so
+wget -P bin/release/linux/x86_64 https://s3.amazonaws.com/jlibtorrent1/release/linux/x86_64/libjlibtorrent.so
 
 # windows
 wget -P bin/release/windows/x86 https://s3.amazonaws.com/jlibtorrent1/release/windows/x86/jlibtorrent.dll
