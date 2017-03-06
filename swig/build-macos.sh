634c1ad3665d87cb6254763b410b18812d1b01aa
Alden Torres <aldenml@gmail.com>
2017-03-06 16:26:44 -0500

    fixed jni build

--- swig/build-macos.sh
+++ swig/build-macos.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 export DEVELOPMENT_ROOT=~/Development
-export BOOST_ROOT=$DEVELOPMENT_ROOT/boost_1_62_0
+export BOOST_ROOT=$DEVELOPMENT_ROOT/boost_1_63_0
 export LIBTORRENT_ROOT=$DEVELOPMENT_ROOT/libtorrent
 export OPENSSL_ROOT=$DEVELOPMENT_ROOT/openssl-macos
 
-$BOOST_ROOT/b2 --user-config=config/macosx-x86_64-config.jam variant=release toolset=clang-x86_64 target-os=darwin location=bin/release/macosx/x86_64
+$BOOST_ROOT/b2 -j8 --user-config=config/macosx-x86_64-config.jam variant=release toolset=darwin-x86_64 target-os=darwin location=bin/release/macosx/x86_64
 strip -S -x bin/release/macosx/x86_64/libjlibtorrent.dylib
 cp bin/release/macosx/x86_64/libjlibtorrent.dylib ../
 
