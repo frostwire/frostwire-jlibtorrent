7118f8fa5defd27ad5bfa2ea7c67a0cd5dd001c8
Alden Torres <aldenml@gmail.com>
2017-11-20 19:35:38 -0500

    using libtorrent master HEAD

--- src/main/java/com/frostwire/jlibtorrent/LibTorrent.java
+++ src/main/java/com/frostwire/jlibtorrent/LibTorrent.java
@@ -45,7 +45,7 @@ public final class LibTorrent {
      * @return the git revision
      */
     public static String revision() {
-        return "30a7b52855ca4f777f55b3e582cca359d6be583d";
+        return "22f8bdfebcc274f95136d408f1d5e1902e0e56d4";
     }
 
     public static int boostVersionNum() {
