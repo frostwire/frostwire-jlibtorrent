452ae9ea35fd62dbac365dcb8843320ac5574e23
Alden Torres <aldenml@gmail.com>
2018-02-17 18:02:15 -0500

    added ByteVector2String unit test

---
+++ src/test/java/com/frostwire/jlibtorrent/VectorsTest.java
@@ -0,0 +1,46 @@
+/*
+ * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
+ *
+ * Licensed under the MIT License.
+ */
+
+package com.frostwire.jlibtorrent;
+
+import com.frostwire.jlibtorrent.swig.byte_vector;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author aldenml
+ * @author gubatron
+ */
+public class VectorsTest {
+
+    @Test
+    public void testByteVector2String() {
+        byte_vector v = new byte_vector();
+        v.push_back((byte) 'A');
+        v.push_back((byte) 'B');
+        v.push_back((byte) 'C');
+        assertEquals(Vectors.byte_vector2string(v, "US-ASCII"), "ABC");
+
+        v.clear();
+        v.push_back((byte) 'A');
+        v.push_back((byte) 'B');
+        v.push_back((byte) 0);
+        assertEquals(Vectors.byte_vector2string(v, "US-ASCII"), "AB");
+
+        v.clear();
+        v.push_back((byte) 0);
+        v.push_back((byte) 0);
+        v.push_back((byte) 0);
+        assertEquals(Vectors.byte_vector2string(v, "US-ASCII"), "");
+
+        v.clear();
+        v.push_back((byte) 'A');
+        v.push_back((byte) 0);
+        v.push_back((byte) 'B');
+        assertEquals(Vectors.byte_vector2string(v, "US-ASCII"), "A");
+    }
+}
