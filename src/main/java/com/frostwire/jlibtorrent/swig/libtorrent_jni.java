98627c52fea5272c509fde2b0925b5a9f0060c6d
Alden Torres <aldenml@gmail.com>
2018-02-25 16:01:28 -0500

    version 1.2.0.17

--- src/main/java/com/frostwire/jlibtorrent/swig/libtorrent_jni.java
+++ src/main/java/com/frostwire/jlibtorrent/swig/libtorrent_jni.java
@@ -13,7 +13,7 @@ public class libtorrent_jni {
 
     public static String jlibtorrentVersion() {
         // extracted from the gradle with the run-swig step
-        return "1.2.0.17-RC2";
+        return "1.2.0.17";
     }
 
     static {
