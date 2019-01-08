03f7554f3a2039b6bb787a5477c4dd2a52f60f26
gubatron <gubatron@gmail.com>
2019-01-08 12:26:56 -0700

    build-macos.sh brought up to speed with travis build
    Now local builds for mac can be achieved for faster testing
    The script has been made so it can be easily modified to support all the other architectures and have faster local builds
    I've included a build-utils.shinc the other build-<os_build>.sh will include

--- swig/build-macos.sh
+++ swig/build-macos.sh
@@ -1,17 +1,47 @@
 #!/usr/bin/env bash
+# This script is meant to run here inside frostwire-jlibtorrent/swig folder
+# It's supposed to be a one step build for the java jar and macosx (.dylib enclosing) jar
+# Output .jar files will be at:
+# frostwire-jlibtorrent/build/libs/jlibtorrent-<version>.jar
+# frostwire-jlibtorrent/build/libs/jlibtorrent-macosx-<version>.jar
 
-export DEVELOPMENT_ROOT=~/Development
-export BOOST_ROOT=$DEVELOPMENT_ROOT/boost_1_68_0
-export LIBTORRENT_ROOT=$DEVELOPMENT_ROOT/libtorrent
-export OPENSSL_ROOT=$DEVELOPMENT_ROOT/openssl-macos
+export LIBTORRENT_REVISION=6724c1eec03caa27f42c48c89367b35d718b8c02
+export OPENSSL_VERSION="1.1.1a"
+export BOOST_VERSION="69"
+export BOOST_MAJOR="1"
+export BOOST_MINOR="0"
 
-$BOOST_ROOT/b2 -j8 --user-config=config/macosx-x86_64-config.jam variant=release toolset=darwin-x86_64 target-os=darwin location=bin/release/macosx/x86_64
-strip -S -x bin/release/macosx/x86_64/libjlibtorrent.dylib
-cp bin/release/macosx/x86_64/libjlibtorrent.dylib ../
+export os_arch=x86_64
+export os_build=macosx
+export SHARED_LIB=libjlibtorrent.dylib
+export SHARED_LIB_FINAL=${SHARED_LIB} # dummy for macosx
 
-export ANDROID_TOOLCHAIN=$DEVELOPMENT_ROOT/android-toolchain-arm
-export OPENSSL_ROOT=$DEVELOPMENT_ROOT/openssl-arm
-export PATH=$ANDROID_TOOLCHAIN/arm-linux-androideabi/bin:$PATH
-#$BOOST_ROOT/b2 -j8 --user-config=config/android-arm-config.jam variant=release toolset=clang-linux-arm target-os=android location=bin/release/android/armeabi-v7a
-#${ANDROID_TOOLCHAIN}/bin/arm-linux-androideabi-objcopy --only-keep-debug bin/release/android/armeabi-v7a/libjlibtorrent.so bin/release/android/armeabi-v7a/libjlibtorrent.so.debug
-#${ANDROID_TOOLCHAIN}/bin/arm-linux-androideabi-strip --strip-unneeded -x -g bin/release/android/armeabi-v7a/libjlibtorrent.so
+export SWIG=`pwd`
+export SRC="${HOME}/src"
+export BOOST_ROOT="${SRC}/boost_1_${BOOST_VERSION}_0"
+export LIBTORRENT_ROOT="${SRC}/libtorrent"
+export CXX=g++
+export CC=gcc
+
+export OPENSSL_SOURCE="$SRC/openssl-${OPENSSL_VERSION}"
+export OPENSSL_ROOT="$SRC/openssl"
+export OPENSSL_NO_OPTS="no-afalgeng no-async no-autoalginit no-autoerrinit no-capieng no-cms no-comp no-deprecated no-dgram no-dso no-dtls no-dynamic-engine no-egd no-engine no-err no-filenames no-gost no-hw no-makedepend no-multiblock no-nextprotoneg no-posix-io no-psk no-rdrand no-sctp no-shared no-sock no-srp no-srtp no-static-engine no-stdio no-threads no-ui-console no-zlib no-zlib-dynamic -fno-strict-aliasing -fvisibility=hidden -Os"
+
+export run_openssl_configure="./Configure darwin64-${os_arch}-cc ${OPENSSL_NO_OPTS} --prefix=${OPENSSL_ROOT}";
+export run_readelf="otool -L bin/release/${os_build}/${os_arch}/${SHARED_LIB}";
+
+export run_bjam="${BOOST_ROOT}/b2 -j8 --user-config=config/${os_build}-${os_arch}-config.jam variant=release toolset=darwin-${os_arch} target-os=darwin location=bin/release/${os_build}/${os_arch}"
+export run_strip="strip -S -x bin/release/${os_build}/${os_arch}/${SHARED_LIB}"
+export run_objcopy="echo dummy run_objcopy for ${os_build} ${os_arch}"
+source build-utils.shinc
+
+create_folder_if_it_doesnt_exist ${SRC}
+prompt_msg "About to prepare BOOST ${BOOST_VERSION}"
+press_any_to_continue
+prepare_boost
+prepare_openssl
+build_openssl
+prepare_libtorrent
+build_libraries
+cleanup_objects
+
