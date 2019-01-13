1105d0d096c0280b3c18920d7cd760fe36f1ff07
gubatron <gubatron@gmail.com>
2019-01-13 11:25:32 -0700

    factored variable definitions to build-utils.shinc

--- swig/build-android-x86.sh
+++ swig/build-android-x86.sh
@@ -3,15 +3,23 @@
 # It's supposed to be a one step build for the java jar and android (.so enclosing) jars (armv7 and x86)
 # Output .jar files will be at:
 # frostwire-jlibtorrent/build/libs/jlibtorrent-<version>.jar
-# frostwire-jlibtorrent/build/libs/jlibtorrent-macosx-<version>.jar
+# frostwire-jlibtorrent/build/libs/jlibtorrent-android-x86-<version>.jar
 
 # remote android-x86 build with travis is available at https://s3.amazonaws.com/gubatron-jlibtorrent/release/android/x86/libjlibtorrent.so
 
-export LIBTORRENT_REVISION=6724c1eec03caa27f42c48c89367b35d718b8c02
-export OPENSSL_VERSION="1.1.1a"
-export BOOST_VERSION="69"
-export BOOST_MAJOR="1"
-export BOOST_MINOR="0"
+source build-utils.shinc
+abort_if_var_unset "LIBTORRENT_REVISION" LIBTORRENT_REVISION
+abort_if_var_unset "OPENSSL_VERSION" OPENSSL_VERSION
+abort_if_var_unset "BOOST_VERSION" BOOST_VERSION
+abort_if_var_unset "BOOST_MAJOR" BOOST_MAJOR
+abort_if_var_unset "BOOST_MINOR" BOOST_MINOR
+abort_if_var_unset "SWIG" SWIG
+abort_if_var_unset "SRC" SRC
+abort_if_var_unset "BOOST_ROOT" BOOST_ROOT
+abort_if_var_unset "LIBTORRENT_ROOT" LIBTORRENT_ROOT
+abort_if_var_unset "OPENSSL_SOURCE" OPENSSL_SOURCE
+abort_if_var_unset "OPENSSL_ROOT" OPENSSL_ROOT
+abort_if_var_unset "OPENSSL_NO_OPTS" OPENSSL_NO_OPTS
 
 export os_arch=x86
 export os_build=android
@@ -19,25 +27,15 @@ export android_api=19
 export SHARED_LIB=libjlibtorrent.so
 export SHARED_LIB_FINAL=${SHARED_LIB} # dummy for macosx
 
-export SWIG=`pwd`
-export SRC="${HOME}/src"
-export BOOST_ROOT="${SRC}/boost_1_${BOOST_VERSION}_0"
-export LIBTORRENT_ROOT="${SRC}/libtorrent"
 export CXX=g++
 export NDK_VERSION=r18b
-source build-utils.shinc
+
 prepare_android_toolchain
 abort_if_var_unset "ANDROID_TOOLCHAIN" ${ANDROID_TOOLCHAIN}
 export CC=$ANDROID_TOOLCHAIN/bin/i686-linux-android-clang
 
-export OPENSSL_SOURCE="$SRC/openssl-${OPENSSL_VERSION}"
-export OPENSSL_ROOT="$SRC/openssl"
-export OPENSSL_NO_OPTS="no-afalgeng no-async no-autoalginit no-autoerrinit no-capieng no-cms no-comp no-deprecated no-dgram no-dso no-dtls no-dynamic-engine no-egd no-engine no-err no-filenames no-gost no-hw no-makedepend no-multiblock no-nextprotoneg no-posix-io no-psk no-rdrand no-sctp no-shared no-sock no-srp no-srtp no-static-engine no-stdio no-threads no-ui-console no-zlib no-zlib-dynamic -fno-strict-aliasing -fvisibility=hidden -Os"
-
 export run_openssl_configure="./Configure linux-elf ${OPENSSL_NO_OPTS} -fPIC -mstackrealign --prefix=${OPENSSL_ROOT}";
 export run_readelf="${ANDROID_TOOLCHAIN}/bin/i686-linux-android-readelf -d bin/release/${os_build}/${os_arch}/${SHARED_LIB}"
-
-
 export run_bjam="${BOOST_ROOT}/b2 -j8 --user-config=config/${os_build}-${os_arch}-config.jam variant=release toolset=clang--${os_arch} target-os=${os_build} location=bin/release/${os_build}/${os_arch}"
 export run_strip="${ANDROID_TOOLCHAIN}/bin/i686-linux-android-strip --strip-unneeded -x -g bin/release/${os_build}/${os_arch}/${SHARED_LIB}"
 export run_objcopy="${ANDROID_TOOLCHAIN}/bin/i686-linux-android-objcopy --only-keep-debug bin/release/${os_build}/${os_arch}/${SHARED_LIB} bin/release/${os_build}/{$os_arch}/${SHARED_LIB}.debug"
