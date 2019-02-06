3c82f63847195f1c73254bd87c3bf8f76765390d
gubatron <gubatron@gmail.com>
2019-02-06 11:07:09 -0700

    run-swig.sh updates
     - Fixed sed calls to be Linux compatible, they were FreeBSD/macos compatible
     - Added notes about possible segfaults running SWIG 3.0.5, and link to swig-3.0.12 .tar.gz download (installable with simple ./configure; make; sudo make install in ubuntu)

--- swig/run-swig.sh
+++ swig/run-swig.sh
@@ -7,22 +7,40 @@ abort_if_var_unset "BOOST_ROOT" ${BOOST_ROOT}
 abort_if_var_unset "LIBRARY_VERSION" ${LIBRARY_VERSION}
 
 function fixCode() {
-    sed -i '' 's/) &;/)  ;/g' ${LIBTORRENT_ROOT}/include/libtorrent/file_storage.hpp
-    sed -i '' 's/) & noexcept;/)   noexcept;/g' ${LIBTORRENT_ROOT}/include/libtorrent/file_storage.hpp
+    uname=`uname -s`
+    if [ "$(uname)" == "Darwin" ]; then
+      # FreeBSD's sed is weird, it needs that extra '' parameter there for some reason
+      sed -i '' 's/) &;/)  ;/g' ${LIBTORRENT_ROOT}/include/libtorrent/file_storage.hpp
+      sed -i '' 's/) & noexcept;/)   noexcept;/g' ${LIBTORRENT_ROOT}/include/libtorrent/file_storage.hpp
+    else
+      sed -i 's/) &;/)  ;/g' ${LIBTORRENT_ROOT}/include/libtorrent/file_storage.hpp
+      sed -i 's/) & noexcept;/)   noexcept;/g' ${LIBTORRENT_ROOT}/include/libtorrent/file_storage.hpp
+    fi
 }
 
 function refixCode() {
-    sed -i '' 's/)  ;/) \&;/g' ${LIBTORRENT_ROOT}/include/libtorrent/file_storage.hpp
-    sed -i '' 's/)   noexcept;/) \& noexcept;/g' ${LIBTORRENT_ROOT}/include/libtorrent/file_storage.hpp
+    uname=`uname -s`
+    if [ "$(uname)" == "Darwin" ]; then   
+      sed -i '' 's/)  ;/) \&;/g' ${LIBTORRENT_ROOT}/include/libtorrent/file_storage.hpp
+      sed -i '' 's/)   noexcept;/) \& noexcept;/g' ${LIBTORRENT_ROOT}/include/libtorrent/file_storage.hpp
+    else
+      sed -i 's/)  ;/) \&;/g' ${LIBTORRENT_ROOT}/include/libtorrent/file_storage.hpp
+      sed -i 's/)   noexcept;/) \& noexcept;/g' ${LIBTORRENT_ROOT}/include/libtorrent/file_storage.hpp	
+    fi
 }
 
 function runJni()
 {
-    JAVA_SRC_OUTPUT=../src/main/java/com/frostwire/jlibtorrent/swig
-
+    export JAVA_SRC_OUTPUT=`pwd`/../src/main/java/com/frostwire/jlibtorrent/swig
+    echo ${JAVA_SRC_OUTPUT}
     rm -rf ${JAVA_SRC_OUTPUT}
     mkdir -p ${JAVA_SRC_OUTPUT}
 
+    # If you're running from Ubuntu and you have swig 3.0.5 you might end up with a segfault running the swig command below
+    #./run-swig.sh: line 33: 46204 Segmentation fault
+    # Make sure your swig command is compiled from source, version 3.0.12 works on mac
+    # https://sourceforge.net/projects/swig/files/swig/swig-3.0.12/swig-3.0.12.tar.gz/download
+    
     swig -c++ -java -o libtorrent_jni.cpp \
         -outdir ${JAVA_SRC_OUTPUT} \
         -package com.frostwire.jlibtorrent.swig \
@@ -63,11 +81,22 @@ function runJni()
     # do, but in practice, these director types are controlled by us
     # and we know we can do it. The main reason is to be able to
     # compile with -fno-rtti.
-    sed -i '' 's/dynamic_cast<SwigDirector_/static_cast<SwigDirector_/g' libtorrent_jni.cpp
+    uname=`uname -s`
+    if [ "$(uname)" == "Darwin" ]; then    
+	sed -i '' 's/dynamic_cast<SwigDirector_/static_cast<SwigDirector_/g' libtorrent_jni.cpp
+    else
+	sed -i 's/dynamic_cast<SwigDirector_/static_cast<SwigDirector_/g' libtorrent_jni.cpp	
+    fi
+    
 
     # replace jlibtorrent version
     GRADLE_VERSION=`sed -n -e '/^version /s/.* //p' ../build.gradle | tr -d "'"`
-    sed -i '' 's/\$LIBRARY_VERSION\$/'"${GRADLE_VERSION}"'/g' ../src/main/java/com/frostwire/jlibtorrent/swig/libtorrent_jni.java
+    uname=`uname -s`
+    if [ "$(uname)" == "Darwin" ]; then      
+	sed -i '' 's/\$LIBRARY_VERSION\$/'"${GRADLE_VERSION}"'/g' ../src/main/java/com/frostwire/jlibtorrent/swig/libtorrent_jni.java
+    else
+	sed -i 's/\$LIBRARY_VERSION\$/'"${GRADLE_VERSION}"'/g' ../src/main/java/com/frostwire/jlibtorrent/swig/libtorrent_jni.java	
+    fi
 }
 
 fixCode
