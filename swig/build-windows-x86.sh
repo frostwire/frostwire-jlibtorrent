0d310f22000aeb605979ac9f0dfc0aa801b1b527
gubatron <gubatron@gmail.com>
2021-02-23 03:08:01 +0000

    [swig/build-windows-x86] no-asm fix for windows x86 build on jlibtorrent-build machine

--- swig/build-windows-x86.sh
+++ swig/build-windows-x86.sh
@@ -12,7 +12,7 @@ os_build=windows
 SHARED_LIB=lib${LIBRARY_NAME}.dll
 CXX=g++
 CC=i686-w64-mingw32-gcc-posix
-run_openssl_configure="./Configure mingw ${OPENSSL_NO_OPTS} --prefix=$OPENSSL_SOURCE/../openssl"
+run_openssl_configure="./Configure mingw ${OPENSSL_NO_OPTS} no-asm --prefix=$OPENSSL_SOURCE/../openssl"
 run_readelf="eval objdump -p bin/release/${os_build}/${os_arch}/jlibtorrent.dll | grep DLL"
 run_bjam="${BOOST_ROOT}/b2 -j8 --user-config=config/${os_build}-${os_arch}-config.jam variant=release toolset=gcc-${os_arch} target-os=${os_build} location=bin/release/${os_build}/${os_arch}"
 run_strip="i686-w64-mingw32-strip --strip-unneeded -x bin/release/${os_build}/${os_arch}/libjlibtorrent.dll"
@@ -23,6 +23,10 @@ prompt_msg "About to prepare BOOST ${BOOST_VERSION}"
 press_any_to_continue
 prepare_boost
 prepare_openssl
+prompt_msg "PATH BEFORE BUILDING=${PATH}"
+CC=i686-w64-mingw32-gcc-posix
+AS=i686-w64-mingw32-as
+press_any_to_continue
 build_openssl
 prepare_libtorrent
 ./run-swig.sh
