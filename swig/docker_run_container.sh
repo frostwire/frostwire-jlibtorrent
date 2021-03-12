af81cb5f16c678ed4ed5004aa8c122a811fec9ae
gubatron <gubatron@gmail.com>
2021-03-11 22:59:33 -0700

    deletions

--- swig/docker_run_container.sh
+++
@@ -1,8 +0,0 @@
-# Mounts this repo's folder as a volume in the container's /frostwire-jlibtorrent create_folder_if_it_doesnt_exist
-# Then executes the build scripts for android
-docker \
- run \
- --cpus 4 \
- -v "$PWD/../../frostwire-jlibtorrent:/frostwire-jlibtorrent" \
- -it jlibtorrent-android \
- /bin/bash /build.sh  #for some reason it won't run the script
