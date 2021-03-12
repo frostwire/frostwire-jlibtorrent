a5af88a517d777b2309a65759c7d5774454432df
gubatron <gubatron@gmail.com>
2021-03-11 22:28:05 -0700

    runs the container and invokes the build.sh script

--- swig/docker_run_container.sh
+++ swig/docker_run_container.sh
@@ -1,3 +1,8 @@
 # Mounts this repo's folder as a volume in the container's /frostwire-jlibtorrent create_folder_if_it_doesnt_exist
 # Then executes the build scripts for android
-docker run -v "$PWD/../../frostwire-jlibtorrent:/frostwire-jlibtorrent" -it jlibtorrent-android #"cd /frostwire-jlibtorrent/swig; ./build-android-arm.sh"
+docker \
+ run \
+ --cpus 4 \
+ -v "$PWD/../../frostwire-jlibtorrent:/frostwire-jlibtorrent" \
+ -it jlibtorrent-android \
+ /bin/bash /build.sh  #for some reason it won't run the script
