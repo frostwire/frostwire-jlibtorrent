53d8294da686d5a97fb670859cd4a3bbc3767448
gubatron <gubatron@gmail.com>
2021-03-12 12:38:44 -0700

    Android ARM and ARM64 now build perfectly on Docker Container

--- swig/build-android-arm.sh
+++ swig/build-android-arm.sh
@@ -10,27 +10,16 @@ export os_arch=arm
 export os_build=android
 export android_api=21
 export SHARED_LIB=lib${LIBRARY_NAME}.so
-#export CXX=g++
-export NDK_VERSION=r21d
 export ANDROID_TOOLCHAIN="/src/android-toolchain-arm"
-
-#export CXX=${ANDROID_TOOLCHAIN}/bin/arm-linux-androideabi-clang++
-export CXX=${ANDROID_TOOLCHAIN}/bin/armv7a-linux-androideabi${android_api}-clang++
-export CC=${ANDROID_TOOLCHAIN}/bin/armv7a-linux-androideabi${android_api}-clang
-
+export CXX=${ANDROID_TOOLCHAIN}/bin/arm-linux-androideabi-clang++
+export CC=${ANDROID_TOOLCHAIN}/bin/arm-linux-androideabi-clang
 android_env
 common_env
 check_min_req_vars
-
 export run_readelf="${ANDROID_TOOLCHAIN}/bin/arm-linux-androideabi-readelf -d bin/release/${os_build}/${os_arch}eabi-v7a/${SHARED_LIB}"
 export run_bjam="${BOOST_ROOT}/b2 -j4 --user-config=config/${os_build}-${os_arch}-config.jam variant=release toolset=clang-linux-${os_arch} target-os=${os_build} location=bin/release/${os_build}/${os_arch}eabi-v7a"
 export run_strip="${ANDROID_TOOLCHAIN}/bin/arm-linux-androideabi-strip --strip-unneeded -x -g bin/release/${os_build}/${os_arch}eabi-v7a/${SHARED_LIB}"
 export run_objcopy="${ANDROID_TOOLCHAIN}/bin/arm-linux-androideabi-objcopy --only-keep-debug bin/release/${os_build}/${os_arch}eabi-v7a/${SHARED_LIB} bin/release/${os_build}/${os_arch}eabi-v7a/${SHARED_LIB}.debug"
 export PATH=${ANDROID_TOOLCHAIN}/arm-linux-androideabi/bin:$PATH;
-sed -i 's/RANLIB = ranlib/RANLIB = "${ANDROID_TOOLCHAIN}\/bin\/arm-linux-androideabi-ranlib"/g' ${BOOST_ROOT}/tools/build/src/tools/gcc.jam;
-create_folder_if_it_doesnt_exist ${SRC}
-prompt_msg "About to prepare BOOST ${BOOST_VERSION}"
-press_any_to_continue
-prepare_libtorrent
-./run-swig.sh
+export BOOST_ROOT=/src/boost_${BOOST_UNDERSCORE_VERSION} && ./run-swig.sh
 build_libraries
