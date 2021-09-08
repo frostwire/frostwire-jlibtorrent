b34e75c0df8a377efdbcab94eef22bfbd6e5fe86
gubatron <gubatron@gmail.com>
2021-09-08 00:50:56 -0600

    build-android-arm.sh n23 sdk updates

--- swig/build-android-arm.sh
+++ swig/build-android-arm.sh
@@ -10,16 +10,17 @@ export os_arch=arm
 export os_build=android
 export android_api=24
 export SHARED_LIB=lib${LIBRARY_NAME}.so
+export NDK_VERSION=r23
 export ANDROID_TOOLCHAIN=/src/android-toolchain-arm
-export PATH=${ANDROID_TOOLCHAIN}/arm-linux-androideabi/bin:${PATH}
-export CXX=${ANDROID_TOOLCHAIN}/bin/arm-linux-androideabi-clang++
-export CC=${ANDROID_TOOLCHAIN}/bin/arm-linux-androideabi-clang
+export PATH=${ANDROID_TOOLCHAIN}/bin:${PATH}
+export CXX=${ANDROID_TOOLCHAIN}/bin/armv7a-linux-androideabi23-clang++
+export CC=${ANDROID_TOOLCHAIN}/bin/armv7a-linux-androideabi23-clang
 android_env
 common_env
 check_min_req_vars
-export run_readelf="${ANDROID_TOOLCHAIN}/bin/arm-linux-androideabi-readelf -d bin/release/${os_build}/${os_arch}eabi-v7a/${SHARED_LIB}"
-export run_bjam="${BOOST_ROOT}/b2 -j8 --user-config=config/${os_build}-${os_arch}-config.jam variant=release toolset=clang-linux-${os_arch} target-os=${os_build} location=bin/release/${os_build}/${os_arch}eabi-v7a"
-export run_strip="${ANDROID_TOOLCHAIN}/bin/arm-linux-androideabi-strip --strip-unneeded -x -g bin/release/${os_build}/${os_arch}eabi-v7a/${SHARED_LIB}"
-export run_objcopy="${ANDROID_TOOLCHAIN}/bin/arm-linux-androideabi-objcopy --only-keep-debug bin/release/${os_build}/${os_arch}eabi-v7a/${SHARED_LIB} bin/release/${os_build}/${os_arch}eabi-v7a/${SHARED_LIB}.debug"
+export run_readelf="${ANDROID_TOOLCHAIN}/bin/llvm-readelf -d bin/release/${os_build}/${os_arch}eabi-v7a/${SHARED_LIB}"
+export run_bjam="${BOOST_ROOT}/b2 -j8 -q --debug-building --user-config=config/${os_build}-${os_arch}-config.jam variant=release toolset=clang-linux-${os_arch} target-os=${os_build} location=bin/release/${os_build}/${os_arch}eabi-v7a"
+export run_strip="${ANDROID_TOOLCHAIN}/bin/llvm-strip --strip-unneeded -x -g bin/release/${os_build}/${os_arch}eabi-v7a/${SHARED_LIB}"
+export run_objcopy="${ANDROID_TOOLCHAIN}/bin/llvm-objcopy --only-keep-debug bin/release/${os_build}/${os_arch}eabi-v7a/${SHARED_LIB} bin/release/${os_build}/${os_arch}eabi-v7a/${SHARED_LIB}.debug"
 export BOOST_ROOT=/src/boost_${BOOST_UNDERSCORE_VERSION} && ./run-swig.sh
 build_libraries
