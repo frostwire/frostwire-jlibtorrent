d79772f0eef004016dd62f9c9d942a4ea4ca9a0a
gubatron <gubatron@gmail.com>
2021-09-08 19:40:02 -0600

    windows x86_64 build updates

--- swig/build-windows-x86_64.sh
+++ swig/build-windows-x86_64.sh
@@ -8,16 +8,16 @@ source build-utils.shinc
 windows_env
 common_env
 check_min_req_vars
-os_arch=x86_64
-os_build=windows
-SHARED_LIB=lib${LIBRARY_NAME}.dll
-CXX=g++
+export os_arch=x86_64
+export os_build=windows
+export SHARED_LIB=lib${LIBRARY_NAME}.dll
 export CXX=x86_64-w64-mingw32-g++-posix
 export CC=x86_64-w64-mingw32-gcc-posix
-run_bjam="${BOOST_ROOT}/b2 -j2 --user-config=config/${os_build}-${os_arch}-config.jam variant=release toolset=gcc-${os_arch} target-os=${os_build} location=bin/release/${os_build}/${os_arch}"
-run_readelf="eval objdump -p bin/release/${os_build}/${os_arch}/jlibtorrent.dll | grep DLL"
-run_strip="x86_64-w64-mingw32-strip --strip-unneeded -x bin/release/${os_build}/${os_arch}/libjlibtorrent.dll";
-run_objcopy="echo dummy run_objcopy for ${os_build} ${os_arch}"
+export run_bjam="${BOOST_ROOT}/b2 -j2 --user-config=config/${os_build}-${os_arch}-config.jam variant=release toolset=gcc-${os_arch} target-os=${os_build} location=bin/release/${os_build}/${os_arch}"
+export run_objcopy="echo dummy run_objcopy for ${os_build} ${os_arch}"
+export run_strip="x86_64-w64-mingw32-strip --strip-unneeded -x bin/release/${os_build}/${os_arch}/libjlibtorrent.dll";
+export run_readelf="eval objdump -p bin/release/${os_build}/${os_arch}/jlibtorrent.dll | grep DLL"
+export run_native_jar="./gradlew nativeWindowsJar"
 prepare_libtorrent
 ./run-swig.sh
 build_libraries
