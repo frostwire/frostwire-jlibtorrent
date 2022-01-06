11ad110cb6504b5684ae882ee6569ac980769796
Angel Leon <gubatron@gmail.com>
2022-01-06 12:24:51 -0700

    openssl 1.1.1m, 1.2.15.1 updates

--- src/main/java/com/frostwire/jlibtorrent/swig/libtorrent_jni.java
+++ src/main/java/com/frostwire/jlibtorrent/swig/libtorrent_jni.java
@@ -13,7 +13,7 @@ public class libtorrent_jni {
 
     public static String jlibtorrentVersion() {
         // extracted from the gradle with the run-swig step
-        return "1.2.14.2";
+        return "1.2.15.1";
     }
 
     static {
