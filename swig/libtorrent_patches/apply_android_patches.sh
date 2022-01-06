ba9cb53aaf4f6b74f4530aa10d9659d3a5559d01
gubatron <gubatron@gmail.com>
2022-01-06 14:06:40 -0700

    no need for nullptr check on enum_net patch. merged into RC_1_2

--- swig/libtorrent_patches/apply_android_patches.sh
+++ swig/libtorrent_patches/apply_android_patches.sh
@@ -6,7 +6,4 @@ git checkout -- .
 # Turns off TORRENT_USE_NETLINK and turns on TORRENT_USE_IFADDRS
 patch -p1 < ifaddr.1.2.14.patch
 
-# https://github.com/arvidn/libtorrent/pull/6465/files
-patch -p1 < enum_net_iface_from_ifaddrs.patch
-
 exit 0
