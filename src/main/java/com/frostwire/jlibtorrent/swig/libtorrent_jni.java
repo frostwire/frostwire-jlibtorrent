aa29c9f858809e22553bf2597ff603b7432e992f
Angel Leon <gubatron@gmail.com>
2022-10-04 00:06:06 -0600

    it builds and all existing tests are passing, need to revise tests related to todo list

--- src/main/java/com/frostwire/jlibtorrent/swig/libtorrent_jni.java
+++ src/main/java/com/frostwire/jlibtorrent/swig/libtorrent_jni.java
@@ -13,7 +13,7 @@ public class libtorrent_jni {
 
     public static String jlibtorrentVersion() {
         // extracted from the gradle with the run-swig step
-        return "1.2.19.0";
+        return "2.0.8.0";
     }
 
     public static boolean isMacOS() {
