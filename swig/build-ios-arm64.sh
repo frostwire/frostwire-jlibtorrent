1af5031a9c6604fead5888dcd42f93b183896499
FunkyMuse <kikoski@protonmail.com>
2023-12-18 23:31:26 +0100

    feat: add iOS target

---
+++ swig/build-ios-arm64.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# This script is meant to run here inside the swig folder
+# It's supposed to be a one-step build for the java jar and iOS (.dylib enclosing) jar
+# Output .jar files will be at:
+# ../build/libs/${LIBRARY_NAME}-<version>.jar
+# ../build/libs/${LIBRARY_NAME}-ios-<version>.jar
+source build-utils.shinc
+ios_env
+common_env
+check_min_req_vars
+export os_arch=arm64
+export os_build=ios
+export SHARED_LIB=lib${LIBRARY_NAME}.dylib
+export RELEASE_SHARED_LIB=lib${LIBRARY_NAME}.${os_arch}.dylib
+export CXX=clang++
+export CC=clang
+export run_openssl_configure="./Configure iphoneos-${os_arch} -fembed-bitcode ${OPENSSL_NO_OPTS} --prefix=${OPENSSL_ROOT}"
+export run_readelf="otool -L bin/release/${os_build}/${os_arch}/${SHARED_LIB}"
+export run_bjam="${BOOST_ROOT}/b2 -j16 --user-config=config/${os_build}-${os_arch}-config.jam variant=release toolset=darwin-${os_arch} target-os=iphone location=bin/release/${os_build}/${os_arch}"
+export run_objcopy="echo dummy run_objcopy for ${os_build} ${os_arch}"
+export run_strip="strip -S -x bin/release/${os_build}/${os_arch}/${SHARED_LIB}"
+export run_native_jar="./gradlew nativeiOSArm64Jar"
+create_folder_if_it_doesnt_exist ${SRC}
+prompt_msg "$0: About to prepare BOOST ${BOOST_VERSION}"
+press_any_to_continue
+prepare_boost
+prepare_openssl
+build_openssl
+prepare_libtorrent
+./run-swig.sh
+build_libraries
