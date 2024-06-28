c2a7e856e7db125f531da55fc1253e60568e4a3b
Angel Leon <gubatron@gmail.com>
2024-06-27 18:05:37 -0600

    swig mapping updates and new mappings

--- src/main/java/com/frostwire/jlibtorrent/swig/add_torrent_params.java
+++ src/main/java/com/frostwire/jlibtorrent/swig/add_torrent_params.java
@@ -304,12 +304,12 @@ public class add_torrent_params {
     libtorrent_jni.add_torrent_params_set_trackers(swigCPtr, this, string_vector.getCPtr(trackers), trackers);
   }
 
-  public sha256_hash_vector_vector get_merkle_trees() {
-    return new sha256_hash_vector_vector(libtorrent_jni.add_torrent_params_get_merkle_trees(swigCPtr, this), true);
+  public SWIGTYPE_p_std__vectorT_std__vectorT_libtorrent__sha256_hash_t_t get_merkle_trees() {
+    return new SWIGTYPE_p_std__vectorT_std__vectorT_libtorrent__sha256_hash_t_t(libtorrent_jni.add_torrent_params_get_merkle_trees(swigCPtr, this), true);
   }
 
-  public void set_merkle_trees(sha256_hash_vector_vector v) {
-    libtorrent_jni.add_torrent_params_set_merkle_trees(swigCPtr, this, sha256_hash_vector_vector.getCPtr(v), v);
+  public void set_merkle_trees(SWIGTYPE_p_std__vectorT_std__vectorT_libtorrent__sha256_hash_t_t v) {
+    libtorrent_jni.add_torrent_params_set_merkle_trees(swigCPtr, this, SWIGTYPE_p_std__vectorT_std__vectorT_libtorrent__sha256_hash_t_t.getCPtr(v));
   }
 
   public void set_piece_priorities2(byte_vector piece_priorities) {
--- src/main/java/com/frostwire/jlibtorrent/swig/peer_alert.java
+++ src/main/java/com/frostwire/jlibtorrent/swig/peer_alert.java
@@ -47,13 +47,12 @@ public class peer_alert extends torrent_alert {
     return libtorrent_jni.peer_alert_message(swigCPtr, this);
   }
 
-  public void setPid(sha1_hash value) {
-    libtorrent_jni.peer_alert_pid_set(swigCPtr, this, sha1_hash.getCPtr(value), value);
+  public void setPid(SWIGTYPE_p_sha1_hash value) {
+    libtorrent_jni.peer_alert_pid_set(swigCPtr, this, SWIGTYPE_p_sha1_hash.getCPtr(value));
   }
 
-  public sha1_hash getPid() {
-    long cPtr = libtorrent_jni.peer_alert_pid_get(swigCPtr, this);
-    return (cPtr == 0) ? null : new sha1_hash(cPtr, false);
+  public SWIGTYPE_p_sha1_hash getPid() {
+    return new SWIGTYPE_p_sha1_hash(libtorrent_jni.peer_alert_pid_get(swigCPtr, this), true);
   }
 
   public tcp_endpoint get_endpoint() {
