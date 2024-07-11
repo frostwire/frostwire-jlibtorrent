2aa619c887294c876881d9de5018519be919ca4b
Angel Leon <gubatron@gmail.com>
2024-07-11 13:28:49 -0600

    Refactor AnnounceEndpoint and add AnnounceInfohash class
    - Simplify AnnounceEndpoint to use a single announce_endpoint field
    - Remove redundant fields and methods from AnnounceEndpoint
    - Add methods for infohashV1() and infohashV2()
    - Introduce new AnnounceInfohash class to handle infohash-specific data
    - Update method implementations to use the new structure

--- src/main/java/com/frostwire/jlibtorrent/AnnounceEndpoint.java
+++ src/main/java/com/frostwire/jlibtorrent/AnnounceEndpoint.java
@@ -14,44 +14,10 @@ import com.frostwire.jlibtorrent.swig.announce_endpoint;
  * @author aldenml
  */
 public class AnnounceEndpoint {
+    private final announce_endpoint h;
 
-    protected String message;
-    protected ErrorCode lastError;
-    protected String localEndpoint;
-    protected long nextAnnounce;
-    protected long minAnnounce;
-    protected int scrapeIncomplete;
-    protected int scrapeComplete;
-    protected int scrapeDownloaded;
-    protected int fails;
-    protected boolean updating;
-    protected boolean isWorking;
-
-    /**
-     * @param e the native object
-     */
-    public AnnounceEndpoint(announce_endpoint e) {
-        init(e);
-    }
-
-    /**
-     * If this tracker has returned an error or warning message
-     * that message is stored here.
-     *
-     * @return the error or warning message
-     */
-    public String message() {
-        return message;
-    }
-
-    /**
-     * If this tracker failed the last time it was contacted
-     * this error code specifies what error occurred.
-     *
-     * @return the last error code
-     */
-    public ErrorCode lastError() {
-        return lastError;
+    public AnnounceEndpoint(announce_endpoint endpoint) {
+        this.h = endpoint;
     }
 
     /**
@@ -59,108 +25,38 @@ public class AnnounceEndpoint {
      *
      * @return the local endpoint
      */
-    public String localEndpoint() {
-        return localEndpoint;
+    public TcpEndpoint localEndpoint() {
+        return new TcpEndpoint(h.getLocal_endpoint());
     }
 
     /**
-     * The time of next tracker announce in milliseconds.
-     *
-     * @return the time of next tracker announce in milliseconds
+     * Set to false to not announce from this endpoint.
      */
-    public long nextAnnounce() {
-        return nextAnnounce;
+    public boolean enabled() {
+        return h.getEnabled();
     }
 
     /**
-     * No announces before this time.
-     */
-    public long minAnnounce() {
-        return minAnnounce;
-    }
-
-    /**
-     * This is either -1 or the scrape information this tracker last
-     * responded with. Incomplete is the current number of downloaders in
-     * the swarm.
+     * Torrents can be announced using multiple info hashes
+     * for different protocol versions.
      * <p>
-     * If this tracker has returned scrape data, these fields are filled in
-     * with valid numbers. Otherwise they are set to -1.
+     * This is for version 1 (SHA1).
      *
-     * @return the number of current downloaders
+     * @return the V1 announce infohash
      */
-    public int scrapeIncomplete() {
-        return scrapeIncomplete;
+    public AnnounceInfohash infohashV1() {
+        return new AnnounceInfohash(h.get_infohash_v1());
     }
 
     /**
-     * This is either -1 or the scrape information this tracker last
-     * responded with. Complete is the current number of seeds in the swarm.
+     * Torrents can be announced using multiple info hashes
+     * for different protocol versions.
      * <p>
-     * If this tracker has returned scrape data, these fields are filled in
-     * with valid numbers. Otherwise they are set to -1.
+     * This is for version 2 (truncated SHA-256).
      *
-     * @return the current number of seeds
+     * @return the V2 announce infohash
      */
-    public int scrapeComplete() {
-        return scrapeComplete;
-    }
-
-    /**
-     * This is either -1 or the scrape information this tracker last
-     * responded with. Downloaded is the cumulative number of completed
-     * downloads of this torrent, since the beginning of time
-     * (from this tracker's point of view).
-     * <p>
-     * If this tracker has returned scrape data, these fields are filled in
-     * with valid numbers. Otherwise they are set to -1.
-     *
-     * @return the cumulative number of completed downloads
-     */
-    public int scrapeDownloaded() {
-        return scrapeDownloaded;
-    }
-
-    /**
-     * The number of times in a row we have failed to announce to this tracker.
-     *
-     * @return the number of fails
-     */
-    public int fails() {
-        return fails;
-    }
-
-    /**
-     * True while we're waiting for a response from the tracker.
-     */
-    public boolean updating() {
-        return updating;
-    }
-
-    /**
-     * Returns true if the last time we tried to announce to this
-     * tracker succeeded, or if we haven't tried yet.
-     */
-    public boolean isWorking() {
-        return isWorking;
-    }
-
-    /**
-     * NOTE: use this with care and only if necessary.
-     *
-     * @param e the native object
-     */
-    protected void init(announce_endpoint e) {
-        message = Vectors.byte_vector2ascii(e.get_message());
-        lastError = new ErrorCode(e.getLast_error());
-        localEndpoint = new TcpEndpoint(e.getLocal_endpoint()).toString();
-        nextAnnounce = e.get_next_announce();
-        minAnnounce = e.get_min_announce();
-        scrapeIncomplete = e.getScrape_incomplete();
-        scrapeComplete = e.getScrape_complete();
-        scrapeDownloaded = e.getScrape_downloaded();
-        fails = e.getFails();
-        updating = e.getUpdating();
-        isWorking = e.is_working();
+    public AnnounceInfohash infohashV2() {
+        return new AnnounceInfohash(h.get_infohash_v2());
     }
 }
