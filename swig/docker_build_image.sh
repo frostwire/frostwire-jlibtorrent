39d8165af7eadfe542cca5122d6760aa85fb63ae
Angel Leon <gubatron@gmail.com>
2024-09-27 08:11:28 -0600

    Improve CPU core detection and add OS validation in docker build script
    - Updated `docker_build_image.sh` to detect the platform using `uname`.
    - On Linux, uses `nproc` for core detection, and on macOS (Darwin), uses `sysctl -n hw.ncpu`.
    - Added a fallback for unsupported operating systems, which will now exit with error code `1` and display an error message.

--- swig/docker_build_image.sh
+++ swig/docker_build_image.sh
@@ -1,1 +1,10 @@
-CORES=$(nproc) && docker build -t jlibtorrent-android --build-arg CORES=${CORES} .
+if [[ "$(uname)" == "Linux" ]]; then
+  export CORES=$(nproc)
+elif [[ "$(uname)" == "Darwin" ]]; then
+  export CORES=$(sysctl -n hw.ncpu)
+else
+  echo "docker_build_image.sh: Unsupported operating system: $(uname)"
+  exit 1
+fi
+
+docker build -t jlibtorrent-android --build-arg CORES=${CORES} .
