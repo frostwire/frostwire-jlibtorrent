1ad320ba17b6fd5d2e4889a5eb1c8667ac67b8fd
Angel Leon <gubatron@gmail.com>
2024-10-22 19:35:11 -0600

    Add unit test for handling `none_t` bdecode_node type in BDecodeReadTest
    - Implemented `testReadNone` method to verify the behavior of bdecode_node when encountering a non-existent key.
    - The test decodes a torrent file and checks the correct type and size for known dictionary entries.
    - Validates that a missing key returns `bdecode_node.type_t.none_t`.
    - Ensured error codes are handled, and memory is cleared to prevent potential garbage collection issues.

--- src/test/java/com/frostwire/jlibtorrent/BDecodeReadTest.java
+++ src/test/java/com/frostwire/jlibtorrent/BDecodeReadTest.java
@@ -34,4 +34,29 @@ public class BDecodeReadTest {
 
         assertEquals("failed to create torrent info: " + ec.message(), ret, 0);
     }
+
+    @Test
+    public void testReadNone() throws IOException {
+        byte[] data = Utils.resourceBytes("test5.torrent");
+
+        byte_vector buffer = Vectors.bytes2byte_vector(data);
+        bdecode_node e = new bdecode_node();
+        error_code ec = new error_code();
+        int ret = bdecode_node.bdecode(buffer, e, ec);
+
+        assertEquals("failed to decode torrent: " + ec.message(), ret, 0);
+
+        bdecode_node n1 = e.dict_find_s("info");
+        assertEquals(n1.type(), bdecode_node.type_t.dict_t);
+        // { 'length': , 'name': , 'piece length': , 'pieces': }
+        assertEquals(n1.dict_size(), 4);
+
+        bdecode_node n2 = e.dict_find_list_s("any-key");
+        assertEquals(n2.type(), bdecode_node.type_t.none_t);
+        // if (n2.type() != bdecode_node.type_t.none_t)
+        //    assertEquals(n2.list_size(), <size>);
+
+        ec.clear();
+        buffer.clear(); // prevents GC
+    }
 }
