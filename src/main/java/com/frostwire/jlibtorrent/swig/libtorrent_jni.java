301f112a53a537c80f47aa9b11ce2793cf4b1937
Angel Leon <gubatron@gmail.com>
2025-07-22 19:54:17 -0600

    libtorrent_java.i: loadJlibtorrentJNIFromClassloaderResource path fixes

--- src/main/java/com/frostwire/jlibtorrent/swig/libtorrent_jni.java
+++ src/main/java/com/frostwire/jlibtorrent/swig/libtorrent_jni.java
@@ -20,7 +20,7 @@ public class libtorrent_jni {
 
     public static String jlibtorrentVersion() {
         // extracted from the gradle with the run-swig step
-        return "2.0.12.0";
+        return "2.0.12.1";
     }
 
     public static boolean isMacOS() {
