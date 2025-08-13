a1b3b5be41a18fc8540b203179372f1b02e0d9bb
Angel Leon <gubatron@gmail.com>
2025-08-13 15:33:19 -0600

    prepare-macos.h: installs SWIG 4.3.1

--- swig/prepare-macos.sh
+++ swig/prepare-macos.sh
@@ -2,12 +2,21 @@
 brew install wget
 brew install pcre2
 
-export SWIG_VERSION=4.2.1
+# DO NOT USE XTool's bison, the swig build uses a -W flag that's not recognized by it
+brew install bison
+export PATH=/opt/homebrew/opt/bison/bin:${PATH}
+
+export SWIG_VERSION=4.3.1
 
 # Function to check if SWIG is installed and return the version
 check_swig_installed() {
-  if command -v swig >/dev/null 2>&1; then
-    INSTALLED_VERSION=$(swig -version | grep "SWIG Version" | awk '{print $3}')
+  # Find the path to the swig executable.
+  swig_path=$(command -v swig 2>/dev/null)
+
+  # Check if a valid path was found and the file exists.
+  if [ -n "$swig_path" ] && [ -x "$swig_path" ]; then
+    INSTALLED_VERSION=$("$swig_path" -version | grep "SWIG Version" | awk '{print $3}')
+
     if [ "$INSTALLED_VERSION" == "$SWIG_VERSION" ]; then
       echo "SWIG version $SWIG_VERSION is already installed."
       return 0
@@ -16,7 +25,7 @@ check_swig_installed() {
       return 1
     fi
   else
-    echo "SWIG is not installed."
+    echo "SWIG is not installed or the path is invalid."
     return 1
   fi
 }
@@ -25,15 +34,26 @@ check_swig_installed() {
 if ! check_swig_installed; then
   echo "Proceeding to download and install SWIG version $SWIG_VERSION..."
   
-  if [ ! -d swig-${SWIG_VERSION} ]; then
+  if [ ! -d "swig-${SWIG_VERSION}" ]; then
     wget -nv --no-check-certificate https://dl.frostwire.com/other/swig/swig-${SWIG_VERSION}.tar.gz
-    tar xzf swig-${SWIG_VERSION}.tar.gz
+    tar xzf "swig-${SWIG_VERSION}.tar.gz"
   fi
   
-  cd swig-${SWIG_VERSION}
-  ./configure --without-d
-  make -j 8
-  sudo make install
+  if [ -d "swig-${SWIG_VERSION}" ]; then
+    cd "swig-${SWIG_VERSION}" || { echo "Failed to change directory to swig-${SWIG_VERSION}"; exit 1; }
+    ./autogen.sh
+    ./configure --without-d
+    make -j 8
+    sudo make install
+    
+    cd ..
+  else
+    echo "Error: Source directory swig-${SWIG_VERSION} not found after extraction."
+    exit 1
+  fi
+
+  # Add /usr/local/bin to PATH for the current shell session
+  export PATH="/usr/local/bin:$PATH"  
   
   # Recheck SWIG installation after build and install
   if check_swig_installed; then
@@ -44,4 +64,3 @@ if ! check_swig_installed; then
 else
   echo "No need to reinstall SWIG."
 fi
-
