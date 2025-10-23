13d22cf4c5b54eb7b214a0633a26a2826dde37bb
gubatron <gubatron@gmail.com>
2025-10-23 00:30:24 +0000

    docker run uses 16 cpus and 100g memory

--- swig/docker_run_interactive.sh
+++ swig/docker_run_interactive.sh
@@ -1,7 +1,7 @@
 # Mounts this repo's folder as a volume in the container's /frostwire-jlibtorrent
 docker \
  run \
- --cpus=8 \
- --memory=32g \
+ --cpus=16 \
+ --memory=100g \
  -v "$PWD/../../frostwire-jlibtorrent:/frostwire-jlibtorrent" \
  -it jlibtorrent-android
